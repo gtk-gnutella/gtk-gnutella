19de0af7e482d7da8e2502ca2cf6b9542e428719
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2001-09-14 18:22:40 +0000

    Fixed typo in prototype of proxy_connect()

--- src/gnutella.h
+++ src/gnutella.h
@@ -568,7 +568,7 @@ void socket_free(struct gnutella_socket *);
 struct gnutella_socket *socket_connect(guint32, guint16, gint);
 struct gnutella_socket *socket_listen(guint32, guint16, gint);
 int connect_socksv5(struct gnutella_socket*);
-int proxy_connect (int, const struct sockaddr *, size_t socklen_t);
+int proxy_connect (int, const struct sockaddr *, socklen_t);
 int recv_socks(struct gnutella_socket*);
 int send_socks(struct gnutella_socket*);
 void socket_monitor_incoming(void);
