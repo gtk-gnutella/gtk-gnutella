cc7aac0ff50ecc139b13cc3eea721bb3ae31c9c8
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2001-10-06 15:06:36 +0000

    Identify yourself as "0.17 alpha" now.

--- src/gnutella.h
+++ src/gnutella.h
@@ -39,8 +39,8 @@
  */
 
 #define GTA_VERSION 0
-#define GTA_SUBVERSION 16
-#define GTA_REVISION "stable"
+#define GTA_SUBVERSION 17
+#define GTA_REVISION "alpha"
 #define GTA_RELEASE "03.10.2001"
 #define GTA_WEBSITE "http://gtk-gnutella.sourceforge.net/"
 
