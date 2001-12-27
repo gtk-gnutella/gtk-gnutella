7aaffc3100f85b1fa81148e6ef2888fe615992d2
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2001-12-27 18:57:08 +0000

    Constant refactoring: move upload-related constants and macros there. Added support for HEAD requests.

--- src/uploads.h
+++ src/uploads.h
@@ -25,8 +25,36 @@ struct upload {
 	gint skip;
 	gint pos;
 	gboolean push;
-
+	gboolean head_only;
 };
+
+/*
+ * Upload states.
+ */
+
+#define GTA_UL_CONNECTED		1	/* Someone has connected to us	*/
+#define GTA_UL_PUSH_RECIEVED	2	/* We got a push request */
+#define GTA_UL_COMPLETE			3	/* The file has been sent completely */
+#define GTA_UL_SENDING			4	/* We are sending data */
+#define GTA_UL_HEADERS			5	/* Receiving the HTTP request headers */
+
+/*
+ * State inspection macros.
+ */
+
+#define UPLOAD_IS_CONNECTING(u)						\
+	(	(u)->status == GTA_UL_HEADERS				\
+	||	(u)->status == GTA_UL_PUSH_RECIEVED	)
+
+#define UPLOAD_IS_COMPLETE(u)	\
+	((u)->status == GTA_UL_COMPLETE)
+
+/*
+ * Until we got all the HTTP headers, the entry does not appear
+ * in the upload list on the GUI.
+ */
+#define UPLOAD_IS_VISIBLE(u) \
+	((u)->status != GTA_UL_HEADERS)
 
 /* 
  * Global Data
@@ -40,9 +68,9 @@ extern guint32 count_uploads;
  * Global Functions
  */
 
-void upload_remove(struct upload *, gchar *);
+void upload_remove(struct upload *, const gchar *, ...);
 void handle_push_request(struct gnutella_node *);
-struct upload *upload_add(struct gnutella_socket *s);
+void upload_add(struct gnutella_socket *s, gboolean head_only);
 void upload_write(gpointer up, gint, GdkInputCondition);
 void upload_close(void);
 
