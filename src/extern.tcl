3afbcbd1fe1d224d409e557b502f8ee234838662
Richard Eckart de Castilho <richard.eckart@googlemail.com>
2002-04-05 20:15:28 +0000

    Take account of changed widget names. Grouped widgets together. Output widget names instead of dots.

--- src/extern.tcl
+++ src/extern.tcl
@@ -9,46 +9,145 @@
 #
 
 set w_main_list {
-	notebook_main
-	clist_menu clist_stats clist_connections button_stats_update
-	sw_menu sw_stats sw_connections
-	clist_nodes button_nodes_add button_nodes_remove entry_host
-	entry_up_connections
-	entry_global_messages entry_global_searches entry_routing_errors entry_dropped_messages
-	entry_count_downloads entry_count_uploads
-	entry_hosts_in_catcher
-	button_host_catcher_clear
-	clist_uploads button_kill_upload button_clear_uploads checkbutton_clear_uploads entry_max_uploads
-	clist_downloads button_abort_download button_resume_download button_clear_download
-	entry_max_downloads entry_max_host_downloads checkbutton_clear_downloads clist_download_queue button_remove_download
-	checkbutton_never_push checkbutton_jump_to_downloads
-	checkbutton_autodownload
-	entry_search button_search label_items_found entry_minimum_speed
-	notebook_search_results button_search_download button_search_stream button_search_clear
-	combo_searches combo_entry_searches
-	button_search_filter button_search_close
-	checkbutton_monitor entry_monitor clist_monitor
-	clist_ul_stats button_ul_stats_clear_deleted button_ul_stats_clear_all
-	checkbutton_enable_search_stats clist_search_stats entry_search_stats_update_interval entry_search_stats_delcoef label_search_stats_count
+	sw_menu
+
+	menu_toolbar_visible
+	menu_statusbar_visible
+	menu_uploads_visible
+	menu_downloads_visible
+	menu_connections_visible
+
+	hbox_statusbar
+
 	button_config_move_path
-	button_config_save_path entry_config_path label_files_scanned entry_config_extensions
-	entry_config_port entry_config_force_ip entry_config_speed checkbutton_config_throttle
+	button_config_rescan_dir
+	button_config_save_path 
+	button_config_update_port 
+	checkbutton_config_force_ip 
+	checkbutton_config_throttle
+	checkbutton_config_proxy_connections
+	entry_config_extensions
+	entry_config_force_ip 
+	entry_config_maxttl 
+	entry_config_myttl
+	entry_config_path 
+	entry_config_port 
+	entry_config_search_items 
+	entry_config_socks_host
+	entry_config_socks_password 
+	entry_config_socks_username 
+	entry_config_speed 
+	radio_config_http 
+	radio_config_socksv4 
+	radio_config_socksv5
+    entry_config_socks_port 
+
+	button_nodes_add 
+	button_nodes_remove 
+	button_host_catcher_clear
+	entry_hosts_in_catcher
+
+	button_uploads_kill
+	button_uploads_clear_completed
+	checkbutton_uploads_auto_clear
+	clist_uploads 	
+
+	button_downloads_abort
+	button_downloads_clear_completed
+	button_downloads_queue_clear
+	button_downloads_resume
+	button_downloads_queue_remove
+	checkbutton_downloads_auto_clear 
+	checkbutton_downloads_never_push
+	clist_downloads 
+	clist_downloads_queue 
+
+	button_search 
+	button_search_clear
+	button_search_close
+	button_search_download 
+	button_search_filter 
+	button_search_stream 
+	checkbutton_search_jump_to_downloads
+	entry_search 
+	entry_search_reissue_timeout
+	entry_search_stats_delcoef 
+	entry_search_stats_update_interval 
+
+	clist_monitor
+	checkbutton_monitor_enable
+
+	popup_search_clear_results
+	popup_search_close
+	popup_search_duplicate 
+	popup_search_filters 
+	popup_search_restart 
+	popup_search_resume
+	popup_search_stop 
+	popup_search_stop_sorting 
+	popup_search_toggle_tabs 
+ 	popup_search 
+
+	popup_hosts 
+	popup_hosts_export
+
+	popup_dl_queued 
+	popup_queue_remove
+	popup_queue_start_now 
+
+	popup_dl_active 
+	popup_downloads_abort
+	popup_downloads_resume
+	popup_downloads_kill 
+	popup_downloads_push 
+	popup_downloads_queue
+
+	popup_monitor 
+	popup_monitor_add_search	
+
+	popup_nodes 
+	popup_nodes_remove
+
+	popup_uploads 
+	popup_uploads_title
+
+	button_ul_stats_clear_all
+	button_ul_stats_clear_deleted 
+	checkbutton_autodownload
+	checkbutton_search_stats_enable 
+	clist_connections 
+	clist_nodes 
+	clist_search_stats 
+	clist_ul_stats 
+	combo_entry_searches
+	combo_searches 
+	ctree_menu 
+	entry_count_downloads 
+	entry_count_uploads
+	entry_dropped_messages
+	entry_global_messages 
+	entry_global_searches 
+	entry_host
+	entry_max_connections 
+	entry_max_downloads 
+	entry_max_host_downloads 
+	entry_max_uploads
+	entry_minimum_speed
+	entry_monitor
+	entry_routing_errors 
+	entry_up_connections
+	hb_toolbar
 	label_current_port
-	checkbutton_config_force_ip entry_config_search_items entry_config_maxttl entry_config_myttl
-	button_config_update_port button_config_rescan_dir
-        label_left label_right entry_config radio_http radio_socksv4 radio_socksv5
-        checkbutton_proxy_connections config_entry_socks_host
-        config_entry_socks_port config_entry_socks_username 
-        config_entry_socks_password entry_max_connections entry_search_reissue_timeout
-	popup_hosts popup_hosts_title popup_hosts_export
-	popup_dl_active popup_dl_active_title download_p_push download_p_queue download_p_kill
-	popup_dl_queued popup_dl_queued_title download_start_now
-	popup_search popup_search_title popup_search_stop_sorting popup_search_filters popup_search_close
-	popup_search_toggle_tabs popup_search_restart popup_search_duplicate popup_search_clear_results
-	popup_search_stop popup_search_resume
-	popup_monitor popup_monitor_title
-	popup_nodes popup_nodes_title
-	popup_uploads popup_uploads_title
+	label_files_scanned 
+	label_items_found 
+	label_search_stats_count
+	notebook_main 
+	notebook_search_results 
+	progressbar_downloads
+	progressbar_uploads 
+	statusbar 
+	sw_connections
+    progressbar_connections 
 }
 
 # interface.h ----------------------------------------------------------------------------------------
@@ -123,11 +222,12 @@ while { ! [eof $s] } {
 		}
 	
 		if { $skip } {
-			puts -nonewline stdout "."
+#			puts -nonewline stdout "."
+			puts stdout "$a"
 			flush stdout
 			set skip 0
 			continue
-		}
+		} 
 	}
 
 	puts $d $l
