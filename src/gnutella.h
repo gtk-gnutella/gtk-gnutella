290ab90755f4e4743aac5f398d51193933a6bdc0
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2002-04-06 07:02:22 +0000

    Starting 0.90 development.

--- src/gnutella.h
+++ src/gnutella.h
@@ -40,11 +40,11 @@
  */
 
 #define GTA_VERSION 0
-#define GTA_SUBVERSION 85
-#define GTA_REVISION "stable"
-#define GTA_REVCHAR ""
+#define GTA_SUBVERSION 90
+#define GTA_REVISION "unstable"
+#define GTA_REVCHAR "u"
 #define GTA_INTERFACE "X11"
-#define GTA_RELEASE "04/04/2002"
+#define GTA_RELEASE "06/04/2002"
 #define GTA_WEBSITE "http://gtk-gnutella.sourceforge.net/"
 
 #define GTA_MSG_INIT					0x00
