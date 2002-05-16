d3966379e2aa3e2b1135330d913c679cbf1f7716
Richard Eckart de Castilho <richard.eckart@googlemail.com>
2002-05-16 20:40:20 +0000

    * Fixed g_return_if_fail in function with gint return value any coding style.

--- src/gtk-missing.c
+++ src/gtk-missing.c
@@ -12,8 +12,8 @@
  * Get position of divider in a GtkPaned. (in GTK2)
  */
 gint gtk_paned_get_position(GtkPaned *paned){
-    g_return_if_fail (paned != NULL);
-    g_return_if_fail (GTK_IS_PANED (paned));
+    g_return_val_if_fail(paned != NULL, -1);
+    g_return_val_if_fail(GTK_IS_PANED (paned), -1);
 
     return paned->child1_size;
 }
