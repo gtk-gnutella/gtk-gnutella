3effe31893a5db775f863998649cdfa95ec69220
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2002-06-29 19:03:37 +0000

    Version of 29/06/2002.

--- src/gnutella.h
+++ src/gnutella.h
@@ -66,7 +66,7 @@
 #define GTA_REVISION "beta"
 #define GTA_REVCHAR "b"
 #define GTA_INTERFACE "X11"
-#define GTA_RELEASE "28/06/2002"
+#define GTA_RELEASE "29/06/2002"
 #define GTA_WEBSITE "http://gtk-gnutella.sourceforge.net/"
 
 #define GTA_MSG_INIT					0x00
