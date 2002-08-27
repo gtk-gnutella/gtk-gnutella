63de1b37f1d5be0f2b3da567b48f0225b12976c5
Richard Eckart de Castilho <richard.eckart@googlemail.com>
2002-08-27 12:11:35 +0000

    * Moved vendor code resolving to vendors.[ch]  * Added ARES to known vendors

---
+++ src/vendors.c
@@ -0,0 +1,125 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2001-2002, Richard Eckart
+ *
+ *----------------------------------------------------------------------
+ * This file is part of gtk-gnutella.
+ *
+ *  gtk-gnutella is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  gtk-gnutella is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with gtk-gnutella; if not, write to the Free Software
+ *  Foundation, Inc.:
+ *      59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *----------------------------------------------------------------------
+ */
+
+#include "vendors.h"
+
+#include <ctype.h>
+
+struct {
+    guint32 code;
+    gchar * name;
+} vendor_map[] = {
+    { T_ARES, "Ares" },
+    { T_BEAR, "BearShare" },
+    { T_CULT, "Cultiv8r" },
+    { T_FIRE, "FireFly" },
+    { T_FISH, "PEERahna" },
+    { T_GNEW, "Gnewtellium" },
+    { T_GNOT, "Gnotella" },
+    { T_GNUC, "Gnucleus" },
+    { T_GNUT, "Gnut" },
+    { T_GTKG, "gtk-gnutella" },
+    { T_HSLG, "Hagelslag" },
+    { T_LIME, "Limewire" },
+    { T_MACT, "Mactella" },
+    { T_MMMM, "Morpheus-v2" },
+    { T_MNAP, "MyNapster" },
+    { T_MRPH, "Morpheus" },
+    { T_MUTE, "Mutella" },
+    { T_NAPS, "NapShare" },
+    { T_OCFG, "OpenCola" },
+    { T_OPRA, "Opera" },
+    { T_PHEX, "Phex" },
+    { T_QTEL, "Qtella" },
+    { T_RAZA, "Shareaza" },
+    { T_SNUT, "SwapNut" },
+    { T_SWAP, "Swapper" },
+    { T_SWFT, "Swift" },
+    { T_TOAD, "ToadNode" },
+    { T_XOLO, "Xolox" },
+    { T_XTLA, "Xtella" },
+    { T_ZIGA, "Ziga" }
+};
+
+#define READ_GUINT32_BE(a,v) { memcpy(&v, a, 4); v = GUINT32_FROM_BE(v); }
+
+
+/*
+ * is_vendor_known:
+ *
+ * Return true is gtk-gnutella knows the given 4-byte vendor code.
+ */
+gboolean is_vendor_known(guchar raw[4])
+{
+    gint n;
+    guint32 code;
+    READ_GUINT32_BE(raw, code);
+
+    if (raw[0] == '0')
+        return FALSE;
+
+    for (n = 0; n < (sizeof(vendor_map)/sizeof(vendor_map[0])); n++)
+        if (code == vendor_map[n].code)
+            return TRUE;
+
+    return FALSE;
+}
+
+/*
+ * lookup_vendor_name
+ *
+ * Return the "human readable" name associated with the 4-byte vendor code.
+ * If we can't understand the code return NULL or if the 4-byte code
+ * consists only of printable characters, return the code as a string.
+ */
+gchar *lookup_vendor_name(guchar raw[4])
+{
+	static gchar temp[5];
+    gint i;
+    guint32 code;
+
+    if (raw[0] == '0')
+        return NULL;
+
+    READ_GUINT32_BE(raw, code);
+
+    for (i = 0; i < (sizeof(vendor_map)/sizeof(vendor_map[0])); i++)
+        if (code == vendor_map[i].code)
+            return vendor_map[i].name;
+
+	/* Unknown type, look whether we have all printable ASCII */
+	for (i = 0; i < sizeof(code); i++) {
+        guchar c = raw[i];
+		if (isascii(c) && isprint(c))
+            temp[i] = c;
+		else {
+            temp[0] = '\0';
+			break;
+		}
+	}
+	temp[4] = '\0';
+
+	return temp[0] ? temp : NULL;
+}
---
+++ src/vendors.h
@@ -0,0 +1,74 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2001-2002, Richard Eckart
+ *
+ *----------------------------------------------------------------------
+ * This file is part of gtk-gnutella.
+ *
+ *  gtk-gnutella is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  gtk-gnutella is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with gtk-gnutella; if not, write to the Free Software
+ *  Foundation, Inc.:
+ *      59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *----------------------------------------------------------------------
+ */
+
+#ifndef __vendors_h__
+#define __vendors_h__
+
+#include <glib.h>
+
+/***
+ *** Known gnutella vendor codes
+ ***/
+#define MAKE_CODE(a,b,c,d) ( \
+	((guint32) (a) << 24) | \
+	((guint32) (b) << 16) | \
+	((guint32) (c) << 8)  | \
+	((guint32) (d)))
+
+#define T_ARES  MAKE_CODE('A','R','E','S')
+#define T_BEAR	MAKE_CODE('B','E','A','R')
+#define T_CULT	MAKE_CODE('C','U','L','T')
+#define T_FIRE	MAKE_CODE('F','I','R','E')
+#define T_FISH	MAKE_CODE('F','I','S','H')
+#define T_GNEW	MAKE_CODE('G','N','E','W')
+#define T_GNOT	MAKE_CODE('G','N','O','T')
+#define T_GNUC	MAKE_CODE('G','N','U','C')
+#define T_GNUT	MAKE_CODE('G','N','U','T')
+#define T_GTKG	MAKE_CODE('G','T','K','G')
+#define T_HSLG	MAKE_CODE('H','S','L','G')
+#define T_LIME	MAKE_CODE('L','I','M','E')
+#define T_MACT	MAKE_CODE('M','A','C','T')
+#define T_MMMM	MAKE_CODE('M','M','M','M')
+#define T_MNAP	MAKE_CODE('M','N','A','P')
+#define T_MRPH	MAKE_CODE('M','R','P','H')
+#define T_MUTE	MAKE_CODE('M','U','T','E')
+#define T_NAPS	MAKE_CODE('N','A','P','S')
+#define T_OCFG	MAKE_CODE('O','C','F','G')
+#define T_OPRA	MAKE_CODE('O','P','R','A')
+#define T_PHEX	MAKE_CODE('P','H','E','X')
+#define T_QTEL	MAKE_CODE('Q','T','E','L')
+#define T_RAZA	MAKE_CODE('R','A','Z','A')
+#define T_SNUT	MAKE_CODE('S','N','U','T')
+#define T_SWAP	MAKE_CODE('S','W','A','P')
+#define T_SWFT	MAKE_CODE('S','W','F','T')
+#define T_TOAD	MAKE_CODE('T','O','A','D')
+#define T_XOLO	MAKE_CODE('X','O','L','O')
+#define T_XTLA	MAKE_CODE('X','T','L','A')
+#define T_ZIGA	MAKE_CODE('Z','I','G','A')
+
+gchar *lookup_vendor_name(guchar code[4]);
+gboolean is_vendor_known(guchar code[4]);
+
+#endif /* __monitor_gui_h__ */
