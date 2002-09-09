c91db572c4826f2695ae24348b086e767e7aa7b5
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2002-09-09 19:39:17 +0000

    Fixed size counting for sent messages: was using the header of the node, not the supplied message type.

--- src/gnet_stats.c
+++ src/gnet_stats.c
@@ -108,7 +108,7 @@ void gnet_stats_count_sent(gnutella_node_t *n, guint8 type, guint32 size)
     stats_pkg_sent[MSG_TOTAL]++;
     stats_pkg_sent[stats_lut[type]]++;
     stats_byte_sent[MSG_TOTAL] += size;
-    stats_byte_sent[stats_lut[n->header.function]] += size;
+    stats_byte_sent[stats_lut[type]] += size;
 }
 
 void gnet_stats_count_expired(gnutella_node_t *n)
