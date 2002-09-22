adfe95a671a3e1353291019d42326a274e1e0ae5
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2002-09-22 22:55:57 +0000

    Version of 22/09/2002.

--- src/common.h
+++ src/common.h
@@ -71,7 +71,7 @@
 #define GTA_REVISION "unstable"
 #define GTA_REVCHAR "u"
 #define GTA_INTERFACE "X11"
-#define GTA_RELEASE "21/09/2002"
+#define GTA_RELEASE "22/09/2002"
 #define GTA_WEBSITE "http://gtk-gnutella.sourceforge.net/"
 
 /*
