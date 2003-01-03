fa02f798ad956fe82e5b07f24c233b042edbadf8
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2003-01-03 21:09:53 +0000

    Added pre-condition to gmsg_check_ggep().

--- src/gmsg.c
+++ src/gmsg.c
@@ -601,6 +601,8 @@ gboolean gmsg_check_ggep(struct gnutella_node *n, gint maxsize, gint regsize)
 	gint len;
 	gint i;
 
+	g_assert(n->size != regsize);
+
 	len = n->size - regsize;				/* Extension length */
 
 	if (len > maxsize) {
