846117fa170d4dfe1aebf8997dccc46237b74dc1
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2003-03-16 17:30:10 +0000

    Version of 16/03/2003.

--- src/common.h
+++ src/common.h
@@ -113,7 +113,7 @@ typedef void (*GCallback) (void);
 #define GTA_REVISION "unstable"
 #define GTA_REVCHAR "u"
 #define GTA_INTERFACE "X11"
-#define GTA_RELEASE "04/03/2003"
+#define GTA_RELEASE "16/03/2003"
 #define GTA_WEBSITE "http://gtk-gnutella.sourceforge.net/"
 
 /*
