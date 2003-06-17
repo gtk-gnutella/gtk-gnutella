45cfdc7a58026e37952ed68cc2cb2f1aa107d6db
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2003-06-17 06:14:38 +0000

    Version of 17/06/2003.

--- src/common.h
+++ src/common.h
@@ -108,10 +108,10 @@ typedef void (*GCallback) (void);
 #define GTA_VERSION 0
 #define GTA_SUBVERSION 92
 #define GTA_PATCHLEVEL 0
-#define GTA_REVISION "stable"
-#define GTA_REVCHAR ""
+#define GTA_REVISION "unstable"
+#define GTA_REVCHAR "u"
 #define GTA_INTERFACE "X11"
-#define GTA_RELEASE "15/06/2003"
+#define GTA_RELEASE "17/06/2003"
 #define GTA_WEBSITE "http://gtk-gnutella.sourceforge.net/"
 
 /* If we compile from CVS (aka "unstable") we want to run from $srcdir
