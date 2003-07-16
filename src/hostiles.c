987e5bd3ff733f7ff46ef0c6299ee5c51d7aa32c
Christian Biere <christianbiere@gmx.de>
2003-07-16 08:59:29 +0000

    Use "sl" for single-linked list. Moved ip/mask parsing to misc.c.

--- src/hostiles.c
+++ src/hostiles.c
@@ -33,6 +33,7 @@
 #include "misc.h"
 
 #include <sys/stat.h>
+#include <ctype.h>
 #include <unistd.h>
 
 RCSID("$Id$");
@@ -58,7 +59,7 @@ static GSList *hostiles_narrow[256];	/* Indexed by FIRST byte */
 void hostiles_retrieve(void)
 {
 	gchar line[1024];
-	gchar *p, *snetmask;
+	gchar *p;
 	guint32 ip, netmask;
 	struct hostile *n;
 	FILE *f;
@@ -77,68 +78,23 @@ void hostiles_retrieve(void)
 	if (!f)
 	   return;
 
-	// XXX code parsing below is somehow a duplicate of that of whitelist.c,
-	// XXX with some enhancements.  Needs to be factorized into misc.c.
-	// XXX	--RAM, 08/05/2003
-
 	while (fgets(line, sizeof(line), f)) {
 		linenum++;
-		if (*line == '#') continue;
-
-		while (*line && line[strlen(line)-1] <= ' ')
-			line[strlen(line)-1] = '\0';
-
-		if (!*line)
+		if (*line == '\0' || *line == '#')
 			continue;
-
-		snetmask = NULL;
-		
-		if ((p = strchr(line, '/')) != NULL) {
+	
+		p = line + strlen(line);	
+		while (isspace((guchar) *(--p)))
 			*p = '\0';
-			snetmask = ++p;
-		}
 
-		/*
-		 * It should only contain IPs and netmasks, but well, we could
-		 * have explict names in there some day...
-		 */
+		if ('\0' == *line)
+			continue;
 
-		ip = host_to_ip(line);
-		if (!ip) {
+		if (!gchar_to_ip_and_mask(line, &ip, &netmask)) {
 			g_warning("hostiles_retrieve(): "
-				"line %d: invalid IP \"%s\"", linenum, line);
+				"line %d: invalid IP or netmask\"%s\"", linenum, line);
 			continue;
 		}
-		
-		if (snetmask) {
-			gint i;
-
-			if (strchr(snetmask, '.'))
-				netmask = gchar_to_ip(snetmask);
-			else {
-				int n = atoi(snetmask);
-				netmask = ~(0xffffffff >> n);
-			}
-			if (!netmask) {
-				netmask = 0xffffffff;
-				g_warning("hostiles_retrieve(): "
-					"line %d: invalid netmask \"%s\", "
-					"using 255.255.255.255 instead.", linenum, snetmask);
-			}
-
-			/*
-			 * Ensure netmask has leading 1's and trailing 0's.
-			 */
-
-			i = highest_bit_set(~netmask);
-			if (netmask != ~((1 << (i+1)) - 1)) {
-				netmask = 0xffffffff;
-				g_warning("hostiles_retrieve(): "
-					"line %d: invalid netmask \"%s\" (bit #%d not 0), "
-					"using 255.255.255.255 instead.", linenum, snetmask, i);
-			}
-		} else
-			netmask = 0xffffffff;
 
 		n = walloc0(sizeof(*n));
 		n->ip_masked = ip & netmask;
@@ -159,7 +115,7 @@ void hostiles_retrieve(void)
  */
 void hostiles_init(void)
 {
-	GSList *l;
+	GSList *sl;
 	gint i;
 
 	hostiles_retrieve();
@@ -182,8 +138,8 @@ void hostiles_init(void)
 	for (i = 0; i < 256; i++)
 		hostiles_exact[i] = hostiles_narrow[i] = NULL;
 
-	for (l = sl_hostiles; l; l = l->next) {
-		struct hostile *h = (struct hostile *) l->data;
+	for (sl = sl_hostiles; sl; sl = g_slist_next(sl)) {
+		struct hostile *h = (struct hostile *) sl->data;
 		if (h->netmask == 0xffffffff) {
 			i = h->ip_masked & 0x000000ff;
 			hostiles_exact[i] = g_slist_prepend(hostiles_exact[i], h);
@@ -203,7 +159,7 @@ void hostiles_init(void)
  */
 void hostiles_close(void)
 {
-	GSList *l;
+	GSList *sl;
 	gint i;
 
 	for (i = 0; i < 256; i++) {
@@ -212,8 +168,8 @@ void hostiles_close(void)
 	}
 	g_slist_free(hostiles_wild);
 
-	for (l = sl_hostiles; l; l = l->next) 
-		wfree(l->data, sizeof(struct hostile));
+	for (sl = sl_hostiles; sl; sl = g_slist_next(sl)) 
+		wfree(sl->data, sizeof(struct hostile));
 
 	g_slist_free(sl_hostiles);
 	sl_hostiles = NULL;
@@ -228,7 +184,7 @@ void hostiles_close(void)
  */
 gboolean hostiles_check(guint32 ip)
 {
-	GSList *l;
+	GSList *sl;
 	struct hostile *h;
 	gint i;
 
@@ -238,8 +194,8 @@ gboolean hostiles_check(guint32 ip)
 
 	i = ip & 0x000000ff;
 
-	for (l = hostiles_exact[i]; l; l = l->next) {
-		h = (struct hostile *) l->data;
+	for (sl = hostiles_exact[i]; sl; sl = g_slist_next(sl)) {
+		h = (struct hostile *) sl->data;
 		if (ip == h->ip_masked)
 			return TRUE;
 	}
@@ -248,8 +204,8 @@ gboolean hostiles_check(guint32 ip)
 	 * Look for a wild match.
 	 */
 
-	for (l = hostiles_wild; l; l = l->next) {
-		h = (struct hostile *) l->data;
+	for (sl = hostiles_wild; sl; sl = g_slist_next(sl)) {
+		h = (struct hostile *) sl->data;
 		if ((ip & h->netmask) == h->ip_masked)
 			return TRUE;
 	}
@@ -260,8 +216,8 @@ gboolean hostiles_check(guint32 ip)
 
 	i = (ip & 0xff000000) >> 24;
 
-	for (l = hostiles_narrow[i]; l; l = l->next) {
-		h = (struct hostile *) l->data;
+	for (sl = hostiles_narrow[i]; sl; sl = g_slist_next(sl)) {
+		h = (struct hostile *) sl->data;
 		if ((ip & h->netmask) == h->ip_masked)
 			return TRUE;
 	}
