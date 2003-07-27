3b2064c39569f272d6bf54ca4e16bfc6e3acf0df
Christian Biere <christianbiere@gmx.de>
2003-07-27 21:45:25 +0000

    gui_record_name_eq(): Don't show the result at a low debug level.

--- src/gui.c
+++ src/gui.c
@@ -250,8 +250,9 @@ gint gui_record_name_eq(gconstpointer rec1, gconstpointer rec2)
     result = g_str_equal(((const record_t *) rec1)->name,
        ((const record_t *) rec2)->name) ? 0 : 1;
 
-    printf("[%s] == [%s] -> %d\n", ((const record_t *) rec1)->name,
-       ((const record_t *) rec2)->name, result);
+	if (common_dbg > 4)
+    	g_message("[%s] == [%s] -> %d\n", ((const record_t *) rec1)->name,
+			((const record_t *) rec2)->name, result);
 
     return result;
 }
