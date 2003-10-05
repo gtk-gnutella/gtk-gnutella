51ec8a4dce4f09f32a5e3a9a469f4c6ae573036e
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2003-10-05 13:10:06 +0000

    Added default Gnutella port define. Version of 05/10/2003.

--- src/common.h
+++ src/common.h
@@ -111,8 +111,10 @@ typedef void (*GCallback) (void);
 #define GTA_REVISION "unstable"
 #define GTA_REVCHAR "u"
 #define GTA_INTERFACE "X11"
-#define GTA_RELEASE "11/09/2003"
+#define GTA_RELEASE "05/10/2003"
 #define GTA_WEBSITE "http://gtk-gnutella.sourceforge.net/"
+
+#define GTA_PORT		6346	/* Default "standard" port */
 
 /*
  * Forbidden glib calls.
