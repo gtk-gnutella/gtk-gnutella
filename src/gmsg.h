f44599f1148402d97265f10cd4b695c177b8c0f8
Christian Biere <christianbiere@gmx.de>
2004-05-24 19:26:43 +0000

    Put '...' and G_GNUC_PRINTF into the same line so that it's easier to verify.

--- src/gmsg.h
+++ src/gmsg.h
@@ -56,9 +56,10 @@ void gmsg_sendto_route(struct gnutella_node *n, struct route_dest *rt);
 gboolean gmsg_can_drop(gpointer pdu, gint size);
 gint gmsg_cmp(gpointer pdu1, gpointer pdu2);
 gchar *gmsg_infostr(gpointer head);
-void gmsg_log_dropped(gpointer head, gchar *reason, ...) G_GNUC_PRINTF(2, 3);
-void gmsg_log_bad(struct gnutella_node *n, gchar *reason, ...)
-	G_GNUC_PRINTF(2, 3); 
+void gmsg_log_dropped(gpointer head,
+	gchar *reason, ...) G_GNUC_PRINTF(2, 3);
+void gmsg_log_bad(struct gnutella_node *n,
+	gchar *reason, ...) G_GNUC_PRINTF(2, 3); 
 
 gboolean gmsg_check_ggep(struct gnutella_node *n, gint maxsize, gint regsize);
 void gmsg_sendto_route_ggep(
