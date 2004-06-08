4fc4f0bb99b2874f77e79da723a4d327a618edec
Christian Biere <christianbiere@gmx.de>
2004-06-08 03:20:15 +0000

    Added const-qualifier to return type of file_info_readable_filename().

--- src/fileinfo.h
+++ src/fileinfo.h
@@ -131,7 +131,7 @@ void file_info_add_source(
 void file_info_remove_source(
     struct dl_file_info *fi, struct download *dl, gboolean discard);
 void file_info_timer(void);
-gchar *file_info_readable_filename(struct dl_file_info *fi);
+const gchar *file_info_readable_filename(struct dl_file_info *fi);
 void file_info_unlink(struct dl_file_info *fi);
 void file_info_upload_stop(struct dl_file_info *fi, const gchar *reason);
 
