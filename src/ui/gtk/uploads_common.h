087edcf25363ef694cc058a31c34c99d84a5bcdc
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2004-08-24 23:22:52 +0000

    Moved from ../../uploads_gui_common.h

---
+++ src/ui/gtk/uploads_common.h
@@ -0,0 +1,38 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2001-2003, Raphael Manfredi, Richard Eckart
+ *
+ *----------------------------------------------------------------------
+ * This file is part of gtk-gnutella.
+ *
+ *  gtk-gnutella is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  gtk-gnutella is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with gtk-gnutella; if not, write to the Free Software
+ *  Foundation, Inc.:
+ *      59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *----------------------------------------------------------------------
+ */
+
+#ifndef _gtk_uploads_common_h_
+#define _gtk_uploads_common_h_
+
+#include "common.h"
+#include "uploads.h"    /* For upload_row_data_t */
+
+gfloat uploads_gui_progress(
+	const gnet_upload_status_t *u, const upload_row_data_t *data);
+const gchar *uploads_gui_status_str(
+    const gnet_upload_status_t *u, const upload_row_data_t *data);
+gboolean upload_should_remove(time_t now, const upload_row_data_t *ul);
+
+#endif /* _gtk_uploads_common_h_ */
