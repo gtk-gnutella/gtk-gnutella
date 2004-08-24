9348ea8c765484867fff7626b764cf452c506347
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2004-08-24 23:09:57 +0000

    Moved from ../tiger.h

---
+++ src/lib/tiger.h
@@ -0,0 +1,37 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2003, Jeroen Asselman
+ *
+ * Tiger hash.
+ *
+ *----------------------------------------------------------------------
+ * This file is part of gtk-gnutella.
+ *
+ *  gtk-gnutella is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  gtk-gnutella is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with gtk-gnutella; if not, write to the Free Software
+ *  Foundation, Inc.:
+ *      59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *----------------------------------------------------------------------
+ */
+
+
+#ifndef _tiger_h_
+#define _tiger_h_
+
+#include <glib.h>
+
+void tiger(guint64 *str, guint64 length, guint64 res[3]);
+
+#endif /* _tiger_h_ */
+
