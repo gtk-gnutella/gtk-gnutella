6728597f9f97609596293022484e22758291900b
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2004-08-24 22:25:32 +0000

    Moved from ../inet.h

---
+++ src/core/inet.h
@@ -0,0 +1,52 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2001-2003, Raphael Manfredi
+ *
+ * Pong caching (LimeWire's ping/pong reducing scheme).
+ *
+ *----------------------------------------------------------------------
+ * This file is part of gtk-gnutella.
+ *
+ *  gtk-gnutella is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  gtk-gnutella is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with gtk-gnutella; if not, write to the Free Software
+ *  Foundation, Inc.:
+ *      59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *----------------------------------------------------------------------
+ */
+
+#ifndef _core_inet_h_
+#define _core_inet_h_
+
+#include <glib.h>
+
+/*
+ * Public interface.
+ */
+
+void inet_init(void);
+
+void inet_firewalled(void);
+void inet_udp_firewalled(void);
+
+gboolean inet_can_answer_ping(void);
+
+void inet_got_incoming(guint32 ip);
+void inet_udp_got_incoming(guint32 ip);
+void inet_udp_got_unsollicited_incoming(guint32 ip);
+
+void inet_connection_attempted(guint32 ip);
+void inet_connection_succeeded(guint32 ip);
+
+#endif /* _core_inet_h_ */
+
