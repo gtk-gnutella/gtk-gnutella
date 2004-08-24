8a48580814a42a1439d72e0e99b5d70cf8ee8c0b
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2004-08-24 22:04:57 +0000

    Moved from ../../gnet_stats_gui_common.h

---
+++ src/ui/gtk/gnet_stats_common.h
@@ -0,0 +1,41 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2001-2003, Raphael Manfredi, Richard Eckart
+ *
+ *----------------------------------------------------------------------
+ * This file is part of gtk-gnutella.
+ *
+ *  gtk-gnutella is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  gtk-gnutella is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with gtk-gnutella; if not, write to the Free Software
+ *  Foundation, Inc.:
+ *      59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *----------------------------------------------------------------------
+ */
+
+#ifndef _gtk_gnet_stats_common_h_
+#define _gtk_gnet_stats_common_h_
+
+#include "gui.h" 
+#include "columns.h"
+#include "if/core/hsep.h"
+
+const gchar *msg_type_str(int MSG_TYPE_COUNT);
+const gchar *msg_drop_str(int MSG_DROP_REASON_COUNT);
+const gchar *general_type_str(int GNR_TYPE_COUNT);
+const gchar *horizon_stat_str(gint row,	c_horizon_t column);
+
+int msg_type_str_size(void);
+void gnet_stats_gui_horizon_update(hsep_triple *table, guint32 triples);
+
+#endif /* _gtk_gnet_stats_common_h_ */
