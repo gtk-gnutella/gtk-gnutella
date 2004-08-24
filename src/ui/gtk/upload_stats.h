d58009c9d00cc3a837bc3c25df6f2e4b931a126d
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2004-08-24 23:18:29 +0000

    Moved from ../../upload_stats_gui.h

---
+++ src/ui/gtk/upload_stats.h
@@ -0,0 +1,35 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2001-2003, Richard Eckart
+ *
+ *----------------------------------------------------------------------
+ * This file is part of gtk-gnutella.
+ *
+ *  gtk-gnutella is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  gtk-gnutella is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with gtk-gnutella; if not, write to the Free Software
+ *  Foundation, Inc.:
+ *      59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *----------------------------------------------------------------------
+ */
+
+#ifndef _gtk_uploads_stats_h_
+#define _gtk_uploads_stats_h_
+
+#include "if/core/uploads.h"
+#include "if/ui/gtk/upload_stats.h"
+
+void upload_stats_gui_init(void);
+void upload_stats_gui_shutdown(void);
+
+#endif /* _gtk_uploads_stats_h_ */
