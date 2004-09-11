6e57e87b4b98ecb7226ad89c2135831a8aa0f9a3
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2004-09-11 12:37:03 +0000

    Forgot to include "if/core/upload_stats.h".

--- src/if/bridge/ui2c.c
+++ src/if/bridge/ui2c.c
@@ -51,6 +51,7 @@
 #include "if/core/search.h"
 #include "if/core/share.h"
 #include "if/core/settings.h"
+#include "if/core/upload_stats.h"
 #include "if/core/uploads.h"
 #include "if/core/upload_stats.h"
 #include "if/core/version.h"
