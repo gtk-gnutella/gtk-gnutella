94650cbb48221af059b6570e5ef833c4d7e24e91
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2004-09-12 09:55:41 +0000

    Renamed vmsg_build_oob_reply_number() into vmsg_build_oob_reply_ind(). Added MUID argument to vmsg_send_oob_reply_ack().

--- src/core/vmsg.h
+++ src/core/vmsg.h
@@ -49,9 +49,9 @@ void vmsg_send_proxy_ack(struct gnutella_node *n, gchar *muid);
 void vmsg_send_qstat_req(struct gnutella_node *n, gchar *muid);
 void vmsg_send_qstat_answer(struct gnutella_node *n, gchar *muid, guint16 hits);
 void vmsg_send_proxy_cancel(struct gnutella_node *n);
-void vmsg_send_oob_reply_ack(struct gnutella_node *n, guint8 wanted);
+void vmsg_send_oob_reply_ack(struct gnutella_node *n, gchar *muid, guint8 want);
 
-struct pmsg *vmsg_build_oob_reply_number(gchar *muid, guint8 hits);
+struct pmsg *vmsg_build_oob_reply_ind(gchar *muid, guint8 hits);
 
 #endif	/* _core_vmsg_h_ */
 
