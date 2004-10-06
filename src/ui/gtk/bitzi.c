3f409f27c17e5b4286923450bb6f126c0f97d06c
Christian Biere <christianbiere@gmx.de>
2004-10-06 05:38:44 +0000

    bitzi_fjtostring(): 	Added const to return type. 	Extended assertion to catch out-of-range values. bitzi_gui_update(): 	Don't pass pointers that are possibly NULL as string to functions 	using format strings. bitzi_gui_get_metadata(): 	Moved here, so that this code can be shared between frontends.

--- src/ui/gtk/bitzi.c
+++ src/ui/gtk/bitzi.c
@@ -62,30 +62,62 @@ static const gchar * const bitzi_fj_table[] =
 	N_("Best Version"),			/* BEST_VERSION*/
 };
 
-gchar *
+const gchar *
 bitzi_fjtostring(bitzi_fj_t fj)
 {
-	g_assert(fj < G_N_ELEMENTS(bitzi_fj_table));
-	return	_(bitzi_fj_table[fj]);
+	g_assert((gint) fj >= 0 && fj < G_N_ELEMENTS(bitzi_fj_table));
+	return	gettext(bitzi_fj_table[fj]);
 }
 
-
 void
-bitzi_gui_update(bitzi_data_t *bitzi_data)
+bitzi_gui_update(const bitzi_data_t *bitzi_data)
 {
+	g_assert(bitzi_data != NULL);
     g_message("bitzi_gui_update: data %p, size %d\n"
 			  "goodness %f, judgement %d, type %s, details %s",
 			bitzi_data,
 			bitzi_data->size,
 			bitzi_data->goodness,
 			bitzi_data->judgement,
-			bitzi_data->mime_type,
-			bitzi_data->mime_desc);
+			NULL_STRING(bitzi_data->mime_type),
+			NULL_STRING(bitzi_data->mime_desc));
 
 	/* Update the various GUI elements */
-	
+
 	search_gui_metadata_update(bitzi_data);
+}
+
+gchar *
+bitzi_gui_get_metadata(const bitzi_data_t *data)
+{
+	g_assert(data != NULL);
 	
+	/*
+	 * Build string
+	 */
+
+	if (data->mime_type) {
+		if (data->mime_desc) {
+			return g_strdup_printf("%s (%1.1f): %s (%s)",
+					bitzi_fjtostring(data->judgement),
+					data->goodness,
+					data->mime_type,
+					data->mime_desc);
+		} else {
+			return g_strdup_printf("%s (%1.1f): %s",
+					bitzi_fjtostring(data->judgement),
+					data->goodness,
+					data->mime_type);
+		}	    
+	} else {
+		if (data->judgement != UNKNOWN) {
+			return g_strdup_printf("%s (%1.1f): No other data",
+					bitzi_fjtostring(data->judgement),
+					data->goodness);
+		}
+	}
+
+	return NULL;
 }
 
 /* vi: set ts=4 sw=4 cindent: */
