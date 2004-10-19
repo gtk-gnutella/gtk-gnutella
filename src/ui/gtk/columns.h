6c1d3ce424c539ba30219fa7ce1c0d7824e9f1be
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2004-10-19 21:37:07 +0000

    Removed the speed, host and SHA1 columns in search results.

--- src/ui/gtk/columns.h
+++ src/ui/gtk/columns.h
@@ -187,11 +187,6 @@ enum {
     c_sr_meta,		/* meta-data info column */
     c_sr_info,
 #define SEARCH_RESULTS_VISIBLE_COLUMNS ((guint) c_sr_info + 1)
-#ifdef USE_GTK1
-    c_sr_speed,
-    c_sr_host,
-    c_sr_sha1,
-#endif /* USE_GTK1 */
 #ifdef USE_GTK2
 	c_sr_fg, /* invisible, holds the foreground color for the row */
 	c_sr_bg, /* invisible, holds the background color for the row */
