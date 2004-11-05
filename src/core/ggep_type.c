2df927692cdbd18ff8b16b675dca863dc9bd4e3b
Christian Biere <christianbiere@gmx.de>
2004-11-05 18:09:59 +0000

    Style fixes. Added ggept_lf_extract() and ggep_lf_encode().

--- src/core/ggep_type.c
+++ src/core/ggep_type.c
@@ -39,15 +39,14 @@ RCSID("$Id$");
 
 #include "lib/override.h"		/* Must be the last header included */
 
-/*
- * ggept_h_sha1_extract
- *
+/**
  * Extract the SHA1 hash of the "H" extension into the supplied buffer.
  *
  * Returns extraction status: only when GGEP_OK is returned will we have
  * the SHA1 in buf.
  */
-ggept_status_t ggept_h_sha1_extract(extvec_t *exv, gchar *buf, gint len)
+ggept_status_t
+ggept_h_sha1_extract(extvec_t *exv, gchar *buf, gint len)
 {
 	gchar tmp[512];
 	gint tlen;
@@ -86,12 +85,11 @@ ggept_status_t ggept_h_sha1_extract(extvec_t *exv, gchar *buf, gint len)
 	return GGEP_OK;
 }
 
-/*
- * ggept_gtkgv1_extract
- *
+/**
  * Extract payload information from "GTKGV1" into `info'.
  */
-ggept_status_t ggept_gtkgv1_extract(extvec_t *exv, struct ggep_gtkgv1 *info)
+ggept_status_t
+ggept_gtkgv1_extract(extvec_t *exv, struct ggep_gtkgv1 *info)
 {
 	gchar tmp[16];
 	gchar *p = tmp;
@@ -120,9 +118,7 @@ ggept_status_t ggept_gtkgv1_extract(extvec_t *exv, struct ggep_gtkgv1 *info)
 	return GGEP_OK;
 }
 
-/*
- * ggept_alt_extract
- *
+/**
  * Extract vector of IP:port alternate locations.
  *
  * The `hvec' pointer is filled with a dynamically allocated vector, and
@@ -131,8 +127,8 @@ ggept_status_t ggept_gtkgv1_extract(extvec_t *exv, struct ggep_gtkgv1 *info)
  *
  * Unless GGEP_OK is returned, no memory allocation takes place.
  */
-ggept_status_t ggept_alt_extract(extvec_t *exv,
-	struct gnutella_host **hvec, gint *hvcnt)
+ggept_status_t
+ggept_alt_extract(extvec_t *exv, struct gnutella_host **hvec, gint *hvcnt)
 {
 	gchar tmp[512];		/* Account for 85 alt-locs -- more than enough! */
 	gchar *p = tmp;
@@ -169,9 +165,7 @@ ggept_status_t ggept_alt_extract(extvec_t *exv,
 	return GGEP_OK;
 }
 
-/*
- * ggept_push_extract
- *
+/**
  * Extract vector of IP:port push proxy locations.
  *
  * The `hvec' pointer is filled with a dynamically allocated vector, and
@@ -180,8 +174,8 @@ ggept_status_t ggept_alt_extract(extvec_t *exv,
  *
  * Unless GGEP_OK is returned, no memory allocation takes place.
  */
-ggept_status_t ggept_push_extract(extvec_t *exv,
-	struct gnutella_host **hvec, gint *hvcnt)
+ggept_status_t
+ggept_push_extract(extvec_t *exv, struct gnutella_host **hvec, gint *hvcnt)
 {
 	gchar tmp[512];		/* Account for 85 entries -- more than enough! */
 	gchar *p = tmp;
@@ -218,14 +212,13 @@ ggept_status_t ggept_push_extract(extvec_t *exv,
 	return GGEP_OK;
 }
 
-/*
- * ggept_hname_extract
- *
+/**
  * Extract hostname of the "HNAME" extension into the supplied buffer.
  * Returns extraction status: only when GGEP_OK is returned will we have
  * extracted something in the supplied buffer.
  */
-ggept_status_t ggept_hname_extract(extvec_t *exv, gchar *buf, gint len)
+ggept_status_t
+ggept_hname_extract(extvec_t *exv, gchar *buf, gint len)
 {
 	gint tlen;
 
@@ -248,3 +241,54 @@ ggept_status_t ggept_hname_extract(extvec_t *exv, gchar *buf, gint len)
 	return GGEP_OK;
 }
 
+ggept_status_t
+ggept_lf_extract(extvec_t *exv, guint64 *filesize)
+{
+	guint64 fs, b = 0;
+	gint i, j, tlen;
+	guint8 buf[63 / 7];
+
+	g_assert(exv->ext_type == EXT_GGEP);
+	g_assert(exv->ext_token == EXT_T_GGEP_LF);
+	
+	tlen = ggep_decode_into(exv, buf, sizeof buf);
+	if (tlen < 1 || tlen > 9)
+		return GGEP_INVALID;
+
+	fs = 0;
+	j = 0;
+	for (i = 0; i < tlen; i++, j += 7) {
+		b = buf[i];
+		if ((b ^= 0x80) & 0x80)
+			return GGEP_INVALID;
+
+		fs |= b << j;
+	}
+
+	if (0 == b && tlen != 1)
+		return GGEP_INVALID;
+
+	if (filesize)
+		*filesize = fs;
+
+	return GGEP_OK;
+}
+
+gint
+ggep_lf_encode(guint64 filesize, guint8 *data)
+{
+  guint64 s = filesize;
+  guint8 *p = data;
+
+  if ((gint64) filesize < 0)
+    return -1;
+  
+  do {
+    *p++ = s | 0x80;
+  } while (0 != (s >>= 7));
+
+  return p - data;
+}
+
+
+/* vi: set ts=4 sw=4 cindent: */
