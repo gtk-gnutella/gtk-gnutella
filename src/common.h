fc7befadf0e44665d8749b4c71f063c109e7db52
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2004-11-12 23:20:04 +0000

    Version of 2004-11-12.

--- src/common.h
+++ src/common.h
@@ -220,7 +220,7 @@ do {				\
 #define GTA_PATCHLEVEL 0
 #define GTA_REVISION "unstable"
 #define GTA_REVCHAR "u"
-#define GTA_RELEASE "2004-11-11"	/* ISO format YYYY-MM-DD */
+#define GTA_RELEASE "2004-11-12"	/* ISO format YYYY-MM-DD */
 #define GTA_WEBSITE "http://gtk-gnutella.sourceforge.net/"
 
 #if defined(USE_GTK1)
