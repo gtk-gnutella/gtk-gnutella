d77afcc5c35d546fc471c8372757b9a11fdc9546
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2004-11-27 16:23:04 +0000

    Added tokens for 0.95.

--- src/core/token.c
+++ src/core/token.c
@@ -50,48 +50,6 @@ RCSID("$Id$");
  * There can be up to 2^5 = 32 keys per version.
  */
 
-static const gchar *keys_092c[] = {
-	"0d69 54ec e06a 47c4 ec25 cb35 4f3a ec74",
-	"c80f 10cd fbd6 85a9 69ef e724 c519 2997",
-	"05e4 401f fd79 0e8e def5 12d6 80a9 53b7",
-	"f7f5 ae0b 2649 1441 eab4 562f 9509 c4b7",
-	"811e 301f 23d0 7e71 017e d449 6c8c 232f",
-	"44f1 2a2b d2da 2313 17df 1a21 635f dea2",
-	"200e 7cfe 35fa 5a6a 47fc f79e 81c6 e11c",
-	"1f7d 541d 1193 4d44 bd84 fdd6 7659 2573",
-	"5db1 b96a 2961 7c83 c254 b19d 75dd 1844",
-	"72ff 61c8 8553 ddd1 9a32 24cc 88bb 51fd",
-	"664d 87d3 1e30 3778 31a2 da87 2e9d f832",
-	"c3d9 6801 e69f cf8d d7c7 4f62 9b80 3438",
-	"d2fc 0fad 1340 e47a 3f3e b012 18fe 3ad0",
-	"2258 65cf 591c dc58 b68a ac2b d174 fe1d",
-	"b6a1 7686 c7f7 9e57 d9e8 6c47 e128 d5c0",
-	"c545 7424 1b25 e586 1f94 e119 25af 2862",
-	"4fb8 1f55 4a5b 2e21 dc48 9fba 7b5c e381",
-	"dfe0 c023 06b6 d236 82f6 5732 40d4 492e",
-	"93d6 d989 aa52 3ca0 8a69 a79a 424d b7a3",
-	"7257 7cff ac09 668f 3b0e 7d6b fe8a 7e7d",
-};
-
-static const gchar *keys_092_2[] = {
-	"b6c1 2fd1 ae02 409f 7f04 f34a 8076 4283",
-	"9232 aa04 652b fa9d 6f48 f3dc b616 ffc1",
-	"211d c9d9 1fa4 6e78 335b c094 dd22 9ac7",
-	"9f5f df94 730b b366 0d6b f4f9 caed 11a9",
-	"3458 9e10 8b23 5977 f5d1 66d7 8bb0 238f",
-	"ec9b f865 fade fa12 1930 6f3e 7dde 47a6",
-	"8e02 5453 4a37 c4c1 f7ab a611 f454 69aa",
-	"e450 8506 06aa 3de9 a7c8 4b09 8427 65d2",
-	"5d19 a84f a371 70f4 464d 0ba7 ff51 a93b",
-	"fe1c fa92 0cc9 46f1 128a 810c 434c 1568",
-	"779d 8c74 99aa 1d50 659e b4cf 47c7 3325",
-	"06bb d901 f3e8 0d06 f77a a20c 31fe 0bc8",
-	"cbff 3cf3 325b 8fc9 bdfc 7acf 15c1 25a2",
-	"f167 81cc be83 60e4 6535 092f ea9d 8ef6",
-	"c2ec 27f2 0b30 5155 3cd4 dc8c 5928 2e63",
-	"d365 afac 948e ffdc abc1 7687 2850 9d58",
-};
-
 static const gchar *keys_093_1[] = {
 	"8bd8 5c21 1f38 b433 f6bb 8b9c d3ed cbdb",
 	"550c 0a1e d6af ba66 11cb 2e38 348a 2cba",
@@ -111,27 +69,27 @@ static const gchar *keys_093_1[] = {
 	"ee48 01f0 40d7 e57b fd0d d3be 84f8 fbe8",
 };
 
-static const gchar *keys_095u[] = {
-	"2f46 2dd9 4806 cf8a 9b5c 8aff bcdb 1bcd",
-	"5a70 0e24 4924 15b8 6f99 de62 15b6 ea58",
-	"2cbe fede 70fb bdf6 1e24 19f0 f656 db55",
-	"2b5a 1130 f91f 9c13 9ec3 0d56 6e09 a111",
-	"ff90 7a78 9b24 cb34 71d3 32e5 3541 d5af",
-	"bc2d cb6c 4bd2 c3c6 a3f8 7b33 32cf 2d46",
-	"4234 cc41 ca94 cf18 e8f0 6f7a 0379 13a9",
-	"102a 6c09 a835 454d 2fda f279 a3a2 5d10",
-	"54dd 2ff7 52f2 6bfc 4cc9 1b97 ef05 10e3",
-	"372f 124b 40ef 8812 b418 4dfc 4643 0007",
-	"5cc9 cd6a 5e64 736c 0a13 c900 3508 5136",
-	"0008 6978 d45b 81ae 8b69 dd51 d2ff 8743",
-	"1060 eba1 2ec2 82af 3128 716f 73d1 46d8",
-	"f034 605b 1f54 68c4 5adc 32e3 ff67 358d",
-	"903e 5405 ab52 3b5e 672f 7d89 b4d1 595c",
-	"83f9 b561 2070 9caf 1b7f 0548 4630 36f6",
-	"4680 7381 a8c4 7994 5f22 d8f4 6db5 c89a",
-	"292d 4921 f7bb e0c0 5c13 721f 62af 5670",
-	"144f 1e7c 0249 3217 936e 24b9 c630 3ee1",
-	"969a 39ec 1650 971b 17d2 294b e75c 1872",
+static const gchar *keys_095[] = {
+	"a4d6 1ffa 2f74 8377 37bc ed80 d041 3976",
+	"659f 19f0 7649 6b98 a9b6 d792 34f2 b020",
+	"394c 56f2 eff2 ff9b aaae 2b42 9fa6 9b21",
+	"3c1a 4be1 fbb3 407f b890 8b48 473a 7efe",
+	"0ce6 62c8 064d 9d01 86ae c74d 94fe 8729",
+	"245d c44d 485c 955c e5dc c4b4 3377 b51a",
+	"a88a 875e f61f 02db bab7 5dab e419 ad96",
+	"882c 6d4e f847 11a5 2c60 d949 1ee5 b837",
+	"9c22 436c e33b 39bb 4074 b292 4137 b6f0",
+	"482f cc27 dc1e d20b 09d1 fab8 7aae 899f",
+	"b80d 4624 d321 acb9 6257 ab7e 9a85 b614",
+	"3079 31b7 04e3 46fb c417 618a 14b6 812f",
+	"512a 3952 b884 fab5 3144 208f 34c7 b666",
+	"1b73 dabc 00bf 6b7e 9d83 bf6a b8b0 cadd",
+	"a07a fd93 7357 e911 4903 6bac 6401 b593",
+	"4372 2239 823d 2f4d 0dd8 cfd2 e755 f870",
+	"06b6 9797 e289 6cd2 15d4 3380 428e 4725",
+	"a23a 0699 0453 73bd 2e7a 879b 6fec 5151",
+	"91c0 086f 1ba3 12b5 9d0a ee3e 0e83 808f",
+	"5f9f 1935 9e39 495d 0873 9036 c6ff eaae",
 };
 
 /* 
@@ -144,26 +102,13 @@ struct tokkey {
 } token_keys[] = {
 	/* Keep this array sorted by increasing timestamp */
 	{
-		{ 0, 92, 0, 'c', 0, 1053813600 },			/* 25/05/2003 */
-		keys_092c, G_N_ELEMENTS(keys_092c),
-	},
-	{
-		{ 0, 92, 2, '\0', 0, 1067209200 },			/* 27/10/2003 */
-		keys_092_2, G_N_ELEMENTS(keys_092_2),
-	},
-	{
 		{ 0, 93, 1, '\0', 0, 1072566000 },			/* 28/12/2003 */
 		keys_093_1, G_N_ELEMENTS(keys_093_1),
 	},
-/* XXX Keep this out because a bug in 0.94 and 0.93 prevents them from
- * XXX validating our level if there are more entries in the level we
- * XXX generate compared to the level they can validate. */
-#if 0	/* Until we decide to release 0.95 */
 	{
-		{ 0, 95, 0, 'u', 0, 1089756000 },			/* 14/07/2004 */
-		keys_095u, G_N_ELEMENTS(keys_095u),
+		{ 0, 95, 0, 'u', 0, 1101510000 },			/* 2004-11-27 */
+		keys_095, G_N_ELEMENTS(keys_095),
 	},
-#endif
 };
 
 /*
