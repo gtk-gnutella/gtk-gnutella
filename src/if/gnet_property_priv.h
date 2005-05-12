ba07ad9fbd5ed4f3d451d77e1a50c168dc2f9539
Christian Biere <christianbiere@gmx.de>
2005-05-12 03:19:01 +0000

    Added core property ``hsep_debug''.

--- src/if/gnet_property_priv.h
+++ src/if/gnet_property_priv.h
@@ -109,6 +109,7 @@ extern const guint32  qrp_debug;
 extern const guint32  routing_debug;
 extern const guint32  ggep_debug;
 extern const guint32  pcache_debug;
+extern const guint32  hsep_debug;
 extern const guint32  track_props;
 extern const gboolean stop_host_get;
 extern const gboolean bws_in_enabled;
