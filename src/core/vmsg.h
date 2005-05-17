1459cea7a928be2d264447891aaa92432880cd88
Christian Biere <christianbiere@gmx.de>
2005-05-17 14:48:51 +0000

    Changed vmsg_send_proxy_ack() to static function.

--- src/core/vmsg.h
+++ src/core/vmsg.h
@@ -47,7 +47,6 @@ void vmsg_send_hops_flow(struct gnutella_node *n, guint8 hops);
 void vmsg_send_tcp_connect_back(struct gnutella_node *n, guint16 port);
 void vmsg_send_udp_connect_back(struct gnutella_node *n, guint16 port);
 void vmsg_send_proxy_req(struct gnutella_node *n, const gchar *muid);
-void vmsg_send_proxy_ack(struct gnutella_node *n, gchar *muid, gint version);
 void vmsg_send_qstat_req(struct gnutella_node *n, gchar *muid);
 void vmsg_send_qstat_answer(struct gnutella_node *n, gchar *muid, guint16 hits);
 void vmsg_send_proxy_cancel(struct gnutella_node *n);
@@ -60,4 +59,4 @@ struct pmsg *vmsg_build_oob_reply_ind(gchar *muid, guint8 hits);
 
 #endif	/* _core_vmsg_h_ */
 
-/* vi: set ts=4: */
+/* vi: set ts=4 sw=4 cindent: */
