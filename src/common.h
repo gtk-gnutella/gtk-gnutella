5a6814c4daa159330e82e23ea4f9d7117c036ad6
Hans de Graaff <hans@degraaff.org>
2005-07-31 17:42:38 +0000

    Release 0.95.4 stable.

--- src/common.h
+++ src/common.h
@@ -262,11 +262,11 @@ do {				\
  */
 
 #define GTA_VERSION 0				/**< major version */
-#define GTA_SUBVERSION 96			/**< minor version */
-#define GTA_PATCHLEVEL 0			/**< patch level or teeny version */
-#define GTA_REVISION "unstable"		/**< unstable, beta, stable */
-#define GTA_REVCHAR "u"			/**< u - unstable, b - beta, none - stable */
-#define GTA_RELEASE "2005-07-29"	/**< ISO 8601 format YYYY-MM-DD */
+#define GTA_SUBVERSION 95			/**< minor version */
+#define GTA_PATCHLEVEL 4			/**< patch level or teeny version */
+#define GTA_REVISION "stable"		/**< unstable, beta, stable */
+#define GTA_REVCHAR ""			/**< u - unstable, b - beta, none - stable */
+#define GTA_RELEASE "2005-07-31"	/**< ISO 8601 format YYYY-MM-DD */
 #define GTA_WEBSITE "http://gtk-gnutella.sourceforge.net/"
 
 #if defined(USE_GTK1)
