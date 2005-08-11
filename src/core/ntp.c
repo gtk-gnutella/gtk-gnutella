11367dabb36e7ce857af78e552d31711b1e988eb
Christian Biere <christianbiere@gmx.de>
2005-08-11 07:00:52 +0000

    ntp_probe(): 	Send the probe to localhost rather than "127.0.0.1".

--- src/core/ntp.c
+++ src/core/ntp.c
@@ -156,7 +156,7 @@ ntp_probe(void)
 	tm_now(&now);
 	ntp_tm_serialize(m.transmit_timestamp, &now);
 
-	to.addr = string_to_host_addr("127.0.0.1");
+	to.addr = name_to_host_addr("localhost");
 	to.port = NTP_PORT;
 
 	if (-1 == s_udp_listen->wio.sendto(&s_udp_listen->wio, &to, &m, sizeof(m)))
