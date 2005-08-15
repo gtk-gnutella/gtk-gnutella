5d658f9c1c23e2112e089a90a9175c7a843e5ca6
Christian Biere <christianbiere@gmx.de>
2005-08-15 15:33:12 +0000

    prepare_browsing(): 	Even better calculation of the buffer size.

--- src/core/uploads.c
+++ src/core/uploads.c
@@ -2370,11 +2370,10 @@ prepare_browsing(gnutella_upload_t *u, header_t *header, gchar *request,
 		!is_strprefix(buf, server_hostname) &&
 		upload_likely_from_browser(header)
 	) {
-		static gchar buf[sizeof("Location: http://:/\r\n") +
-			UINT16_DEC_BUFLEN + MAX_HOSTLEN];
+		static const gchar fmt[] = "Location: http://%s:%u/\r\n;
+		static gchar buf[sizeof fmt + UINT16_DEC_BUFLEN + MAX_HOSTLEN];
 
-		gm_snprintf(buf, sizeof buf, "Location: http://%s:%u/\r\n",
-			server_hostname, listen_port);
+		gm_snprintf(buf, sizeof buf, fmt, server_hostname, listen_port);
 
 		g_assert(hevcnt < hevlen);
 		hev[hevcnt].he_type = HTTP_EXTRA_LINE;
