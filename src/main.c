a3d092d0de3f4e651f29eb85a01ef8025858efeb
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2005-08-27 12:42:40 +0000

    Fixed too long a line.

--- src/main.c
+++ src/main.c
@@ -127,7 +127,7 @@
 
 RCSID("$Id$");
 
-#define SLOW_UPDATE_PERIOD		20	/**< Updating period for `main_slow_update' */
+#define SLOW_UPDATE_PERIOD		20	/**< Update period for `main_slow_update' */
 #define EXIT_GRACE				30	/**< Seconds to wait before exiting */
 #define ATEXIT_TIMEOUT			20	/**< Final cleanup must not take longer */
 
