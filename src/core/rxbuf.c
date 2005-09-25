9bcc95ddda43d24371d5698ac0aa376dea5c65fd
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2005-09-25 21:41:48 +0000

    Now merely wraps palloc() and pfree().

--- src/core/rxbuf.c
+++ src/core/rxbuf.c
@@ -42,6 +42,7 @@ RCSID("$Id$");
 
 #include "if/gnet_property_priv.h"
 
+#include "lib/palloc.h"
 #include "lib/override.h"		/* Must be the last header included */
 
 /*
@@ -50,14 +51,13 @@ RCSID("$Id$");
  * is held into a list.  Each pdata_t is equipped with a suitable free routine.
  */
 
-#define BUF_COUNT	1		/**< Initial amount of buffers in pool */
+#define BUF_COUNT	5		/**< Max amount of buffers we want in pool */
 #define BUF_SIZE	4096	/**< Size of each buffer */
 
-static GSList *sl_buffers = NULL;
-static gint allocated = 0;
+static pool_t *rxpool;
 
 /**
- * Put RX buffer back to freelist.
+ * Put RX buffer back to its pool.
  *
  * Can be called directly, or via pdata_unref() because this routine is
  * installed as the "free routine" of the buffer.
@@ -66,50 +66,21 @@ void
 rxbuf_free(gpointer p, gpointer unused_data)
 {
 	(void) unused_data;
-	sl_buffers = g_slist_prepend(sl_buffers, p);
+
+	pfree(rxpool, p);
 }
 
 /**
- * Allocate new RX buffer.
- */
-static pdata_t *
-rxbuf_alloc(void)
-{
-	gchar *phys = g_malloc(BUF_SIZE);
-
-	allocated++;
-
-	return pdata_allocb(phys, BUF_SIZE, rxbuf_free, NULL);
-}
-
-/**
+ * Get a new RX buffer from the pool.
+ *
  * @return new RX buffer.
  */
 pdata_t *
 rxbuf_new(void)
 {
-	pdata_t *buf;
+	gchar *phys = palloc(rxpool);
 
-	/*
-	 * Get first buffer from the head of the free list, in any.
-	 */
-
-	if (sl_buffers) {
-		buf = (pdata_t *) sl_buffers->data;
-		sl_buffers = g_slist_remove(sl_buffers, buf);
-		return buf;
-	}
-
-	/*
-	 * Must allocate a new buffer.
-	 */
-
-	buf = rxbuf_alloc();
-
-	if (dbg > 4)
-		printf("Allocated new RX buffer (#%d)\n", allocated);
-
-	return buf;
+	return pdata_allocb(phys, BUF_SIZE, rxbuf_free, NULL);
 }
 
 /**
@@ -118,12 +89,7 @@ rxbuf_new(void)
 void
 rxbuf_init(void)
 {
-	gint i;
-
-	for (i = 0; i < BUF_COUNT; i++) {
-		pdata_t *buf = rxbuf_alloc();
-		rxbuf_free(buf, NULL);
-	}
+	rxpool = pool_create(BUF_SIZE, BUF_COUNT);
 }
 
 /**
@@ -132,20 +98,7 @@ rxbuf_init(void)
 void
 rxbuf_close(void)
 {
-	GSList *l;
-	gint freed = 0;
-
-	for (l = sl_buffers; l; l = l->next) {
-		pdata_t *buf = (pdata_t *) l->data;
-		G_FREE_NULL(buf);
-		freed++;
-	}
-
-	g_slist_free(sl_buffers);
-
-	if (freed != allocated)
-		g_warning("allocated %d RX buffers, but freed only %d",
-			allocated, freed);
+	pool_free(rxpool);
 }
 
 /* vi: set ts=4 sw=4 cindent: */
