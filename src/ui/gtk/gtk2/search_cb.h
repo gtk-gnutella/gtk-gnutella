6bd6fffcbc3652632119316bf24781168e98b94e
Christian Biere <christianbiere@gmx.de>
2005-10-26 11:47:39 +0000

    Updated prototypes.

--- src/ui/gtk/gtk2/search_cb.h
+++ src/ui/gtk/gtk2/search_cb.h
@@ -34,12 +34,12 @@ void on_search_notebook_switch(GtkNotebook * notebook, GtkNotebookPage * page, g
 void on_search_notebook_focus_tab(GtkNotebook * notebook, GtkNotebookTab arg1, gpointer user_data);
 void on_tree_view_search_select_row(GtkTreeView * tree_view, gpointer user_data);
 void on_search_selected(GtkItem * i, gpointer data);
-void on_button_search_clicked (GtkButton *button, gpointer user_data);
-void on_entry_search_activate (GtkEditable *editable, gpointer user_data);
-void on_entry_search_changed (GtkEditable *editable, gpointer user_data);
+void on_button_search_clicked(GtkButton *button, gpointer user_data);
+void on_combobox_search_activate(GtkWidget *editable, gpointer user_data);
+void on_combobox_search_changed(GtkWidget *widget, gpointer user_data);
 void on_button_search_clear_clicked(GtkButton * button, gpointer user_data);
-void on_button_search_close_clicked (GtkButton *button, gpointer user_data);
-void on_button_search_download_clicked (GtkButton *button, gpointer user_data);
+void on_button_search_close_clicked(GtkButton *button, gpointer user_data);
+void on_button_search_download_clicked(GtkButton *button, gpointer user_data);
 
 gboolean on_tree_view_search_results_key_press_event(GtkWidget *widget, GdkEventKey *event, gpointer user_data);
 gboolean on_tree_view_search_button_press_event(GtkWidget *widget,
