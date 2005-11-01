44e35f59713259da8c30973289dc712afa3086eb
Christian Biere <christianbiere@gmx.de>
2005-11-01 07:58:41 +0000

    Hide GTK1-specific prototypes for GTK2.

--- src/ui/gtk/settings_cb.h
+++ src/ui/gtk/settings_cb.h
@@ -55,9 +55,11 @@ void on_entry_server_hostname_changed(GtkEditable *editable, gpointer user_data)
 
 gboolean on_entry_dbg_property_pattern_focus_out_event(GtkWidget *widget, GdkEventFocus *event, gpointer user_data);
 void on_entry_dbg_property_pattern_activate(GtkEditable *editable, gpointer user_data);
+
+#ifdef USE_GTK1
 void on_entry_dbg_property_value_activate(GtkEditable *editable, gpointer user_data);
-void on_clist_dbg_property_select_row(GtkCList *clist, gint row, gint unused_column,
-	GdkEvent *unused_event, gpointer unused_udata);
+void on_clist_dbg_property_select_row(GtkCList *clist, gint row, gint unused_column, GdkEvent *unused_event, gpointer unused_udata);
+#endif /* USE_GTK1*/
 
 void on_menu_statusbar_visible_activate(GtkMenuItem * menuitem, gpointer user_data);
 void on_menu_toolbar_visible_activate(GtkMenuItem * menuitem, gpointer user_data);
