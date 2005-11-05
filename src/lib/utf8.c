2f2e886951404a9cca471c2d4f8ca11d50087355
Christian Biere <christianbiere@gmx.de>
2005-11-05 23:51:40 +0000

    * Disabled regression checks for this revision.

--- src/lib/utf8.c
+++ src/lib/utf8.c
@@ -1439,7 +1439,7 @@ locale_init(void)
 	 * Skip regression_checks() if the current revision is known
 	 * to be alright.
 	 */
-	if (!is_strprefix(get_rcsid(), "Id: utf8.c,v 1.83 "))
+	if (!is_strprefix(get_rcsid(), "Id: utf8.c,v 1.85 "))
 		regression_checks();
 
 	locale_init_passed = TRUE;
