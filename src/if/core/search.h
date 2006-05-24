99f019312cf777a022f5f52864a348820fcb5759
Christian Biere <christianbiere@gmx.de>
2006-05-24 04:08:26 +0000

    * Added SEARCH_F_LITERAL for preventing interpreting the search string.

--- src/if/core/search.h
+++ src/if/core/search.h
@@ -43,7 +43,8 @@ typedef guint32 gnet_search_t;
 enum {
 	SEARCH_F_PASSIVE 	= 1 << 0,	/**< Start a passive ssearch */
 	SEARCH_F_ENABLED 	= 1 << 1,	/**< Start an enabled search */
-	SEARCH_F_BROWSE		= 1 << 2	/**< Start a browse-host search */
+	SEARCH_F_BROWSE		= 1 << 2,	/**< Start a browse-host search */
+	SEARCH_F_LITERAL	= 1 << 3	/**< Don't parse the query string */
 };
 
 /*
