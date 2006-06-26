e1955dbc233606302d109572eb70ad963589a04a
Christian Biere <christianbiere@gmx.de>
2006-06-26 19:51:00 +0000

    * Use a larger read buffer.  * Handle error from read().  * Use g_message() instead of printf().  * Removed unnecessary casts.

--- src/core/verify_tth.c
+++ src/core/verify_tth.c
@@ -107,17 +107,16 @@ struct tt_file_to_hash_s {
 };
 
 typedef struct tt_computation_context_s tt_computation_context_t;
+
 struct tt_computation_context_s {
 	gint fd;					/**< Handle to the file we are computing. */
 	tt_file_to_hash_t *file;	/**< The file we are computing */
 
 	TT_CONTEXT *tt_ctx;
 	gchar *buffer;
-
+	size_t buffer_size;
 	time_t start;
-
-	gint64 dataread;
-
+	filesize_t dataread;
 	hashtree	*tt_node;
 };
 
@@ -129,7 +128,7 @@ tt_computation_context_free(gpointer u)
 	if (ctx->tt_node != NULL) {
 		hashtree_destroy(ctx->tt_node);
 		wfree(ctx->tt_ctx, sizeof(*ctx->tt_ctx));
-		wfree(ctx->buffer, BLOCKSIZE + 1);
+		wfree(ctx->buffer, ctx->buffer_size);
 	}
 	wfree(ctx, sizeof(*ctx));
 }
@@ -137,10 +136,8 @@ tt_computation_context_free(gpointer u)
 static bgret_t
 tigertree_step_compute(gpointer h, gpointer u, gint ticks)
 {
-	gint i;
+	tt_computation_context_t *ctx = u;
 	ssize_t r;
-	gpointer hash;
-	tt_computation_context_t *ctx = (tt_computation_context_t *) u;
 
 	(void) ticks;
 
@@ -150,15 +147,15 @@ tigertree_step_compute(gpointer h, gpointer u, gint ticks)
 		}
 
 		ctx->dataread = 0;
-		ctx->file = (tt_file_to_hash_t *) g_list_first(files_to_hash)->data;
+		ctx->file = g_list_first(files_to_hash)->data;
 
-		g_warning("[tiger tree] Trying to hash %s", ctx->file->file_name);
+		g_message("[tiger tree] Trying to hash %s", ctx->file->file_name);
+
 		ctx->fd = open(ctx->file->file_name, O_RDONLY);
-
 		if (ctx->fd < 0) {
 			g_warning("[tiger tree] "
 				  "Could not open %s for tigertree hashing: %s",
-			ctx->file->file_name, g_strerror(errno));
+				ctx->file->file_name, g_strerror(errno));
 
 			files_to_hash = g_list_remove(files_to_hash, ctx->file);
 
@@ -176,14 +173,22 @@ tigertree_step_compute(gpointer h, gpointer u, gint ticks)
 	}
 
 	*ctx->buffer = 0x00;	/* Leaf hash */
-	if ((r = read(ctx->fd, ctx->buffer + 1, BLOCKSIZE)) < 0)
-		printf("Error while reading file\n");
+	r = read(ctx->fd, ctx->buffer + 1, ctx->buffer_size - 1);
+	if ((ssize_t) -1 == r) {
+		if (is_temporary_error(errno)) {
+			return BGR_MORE;
+		} else {
+			g_message("Error while reading file: %s", g_strerror(errno));
+			return BGR_ERROR;
+		}
+	}
 
 	ctx->dataread += r;
 
-	printf("Read %f\r", (float)ctx->dataread / (1024.0 * 1024.0));
 	/* Check wether we read data first, before trying to hash it */
 	if (r > 0 || ctx->dataread == 0) {
+		gpointer hash;
+
 		g_assert(ctx->buffer[0] == 0x00);
 
 		hash = g_malloc(TIGERSIZE + 1);
@@ -196,36 +201,25 @@ tigertree_step_compute(gpointer h, gpointer u, gint ticks)
 	}
 
 	if (r < BLOCKSIZE) {
-		static gchar digest_b32[39 + 1];
-		gchar cur_hash[TIGERSIZE];
+		struct tth cur_hash;
 
-		printf("[tigertree] Done %d\n", (gint) r);
+		tt_digest(ctx->tt_ctx, cur_hash.data);
 
-		tt_digest(ctx->tt_ctx, cur_hash);
+		g_message("TT hash for '%s': %s",
+			ctx->file->file_name, tth_base32(&cur_hash));
 
-		printf("TT hash for '%s':          ", ctx->file->file_name);
-
-  		for (i = 0; i<TIGERSIZE; i++) {
-			printf("%.2X", (guchar) cur_hash[i]);
-	  	}
-
-		printf("  TT blocks processed: %s, index: %d\n",
+		g_message("  TT blocks processed: %s, index: %d",
 			uint64_to_string(ctx->tt_ctx->count), ctx->tt_ctx->index);
 
 		hashtree_finish(ctx->tt_node);
 
-		printf("Calculated hash:  ");
- 		for (i = 0; i < TIGERSIZE; i++) {
-			printf("%.2X", ((guchar *)ctx->tt_node->parent->hash)[i]);
+		{
+			struct tth tth;
+			
+			memcpy(tth.data, ctx->tt_node->parent->hash, sizeof tth.data);
+			g_message("Calculated hash: %s", tth_base32(&tth));
+			g_message("  TT depth %d", ctx->tt_node->depth);
 		}
-		printf("  TT depth %d\n",
-			ctx->tt_node->depth);
-
-		base32_encode_into(ctx->tt_node->parent->hash, TIGERSIZE,
-			digest_b32, sizeof(digest_b32));
-		digest_b32[SHA1_BASE32_SIZE] = '\0';
-
-		printf("Base 32: %s\n", digest_b32);
 
 		hashtree_destroy(ctx->tt_node);
 		ctx->tt_node = NULL;
@@ -273,7 +267,8 @@ request_tigertree(struct shared_file *sf)
 		ctx = walloc0(sizeof(*ctx));
 		ctx->fd = -1;
 		ctx->tt_ctx = walloc0(sizeof(*ctx->tt_ctx));
-		ctx->buffer = walloc(BLOCKSIZE + 1);
+		ctx->buffer_size = 32 * BLOCKSIZE + 1;
+		ctx->buffer = walloc(ctx->buffer_size);
 
 		ctx->tt_node = hashtree_new(tt_internal_hash);
 
@@ -303,24 +298,26 @@ tt_parse_header(struct download *d, header_t *header)
 	buf = strchr(buf, ';');
 
 	if (buf == NULL) {
-		printf("Incorrect X-Thex-Uri, trying to work around\n");
+		static const gchar prefix[] = "urn:tree:tiger/:";
+
+		g_message("Incorrect X-Thex-Uri, trying to work around");
 		buf = header_get(header, "X-Thex-Uri");
-		buf = strstr(buf, "urn:tree:tiger/:");
-		if (buf != NULL)
-			buf += sizeof("urn:tree:tiger/:") - 1;	/* Portable? YES--RAM*/
-	} else
-	{
+		buf = strstr(buf, prefix);
+		if (buf != NULL) {
+			buf += CONST_STRLEN(prefix);
+		}
+	} else {
 		buf++;	/* Skip ; */
 	}
 
 	if (buf == NULL) {
-		printf("Could not find tigertree %s", buf);
+		g_message("Could not find tigertree %s", buf);
 		return;
 	}
 
 	memcpy(hash, buf, 39);
 	hash[39] = '\0';
-	printf("Tigertree value is %s\n", hash);
+	g_message("Tigertree value is %s\n", hash);
 
 #if 0
 	fi = file_info_get(hash, save_file_path, 0, NULL);
