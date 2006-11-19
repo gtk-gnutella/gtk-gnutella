bcbabfcab6c28273d8d079bebb8cc36e20f1c62f
Christian Biere <christianbiere@gmx.de>
2006-11-19 11:57:16 +0000

    * Added support for spectacular crashes.

---
+++ src/lib/crash.c
@@ -0,0 +1,132 @@
+/*
+ * Copyright (c) 2006 Christian Biere <christianbiere@gmx.de>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the authors nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/**
+ * @ingroup lib
+ * @file
+ *
+ * A simple crash handler.
+ *
+ * @author Christian Biere
+ * @date 2006
+ */
+
+#include "common.h"
+
+RCSID("$Id$")
+
+#include "lib/misc.h"
+#include "lib/vmm.h"
+
+#include "lib/override.h"		/* Must be the last header included */
+
+static const char *exec_pathname;	/* The file to execute. */
+static const char *orig_argv0;		/* The original argv[0]. */
+
+static const int signals[] = {
+#ifdef SIGBUS
+	SIGBUS,
+#endif
+#ifdef SIGTRAP
+	SIGTRAP,
+#endif
+	SIGABRT,
+	SIGFPE,
+	SIGILL,
+	SIGSEGV
+};
+
+static void
+crash_handler(int signo)
+{
+	static const char commands[] = "bt full\nkill\nquit\n";
+	char const *argv[16];
+	char pid_buf[32], *pid_ptr;
+	sigset_t oset;
+	int fds[2];
+	pid_t pid;
+	unsigned i;
+
+	(void) signo;
+
+	for (i = 0; i < G_N_ELEMENTS(signals); i++) {
+		set_signal(signals[i], SIG_DFL);
+	}
+
+	pid = getpid();
+	pid_ptr = &pid_buf[sizeof pid_buf - 1];
+	*pid_ptr = '\0';
+	do {
+		*--pid_ptr = (pid % 10) + '0';
+		pid /= 10;
+	} while (pid && pid_ptr != pid_buf);
+
+	if (0 == pipe(fds)) {
+		close(STDIN_FILENO);
+		dup(fds[0]);
+		write(fds[1], commands, sizeof commands - 1);
+		argv[0] = exec_pathname;
+		argv[1] = "-q";
+		argv[2] = "-n";
+		argv[3] = orig_argv0;
+		argv[4] = pid_ptr;
+		argv[5] = NULL;
+
+		pid = vfork();
+		if (0 == pid) {
+			execve(argv[0], (const void *) argv, NULL);
+			_exit(EXIT_FAILURE);
+		}
+	}
+	sigprocmask(SIG_BLOCK, NULL, &oset);
+	sigsuspend(&oset);
+	abort();
+}
+
+/**
+ * Installs a simple crash handler.
+ * 
+ * @param pathname The pathname of the program to execute on crash.
+ * @param argv0 The original argv[0] from main().
+ */
+void
+crash_init(const char *pathname, const char *argv0)
+{
+	unsigned i;
+
+	exec_pathname = prot_strdup(pathname);
+	orig_argv0 = prot_strdup(argv0);
+
+	for (i = 0; i < G_N_ELEMENTS(signals); i++) {
+		set_signal(signals[i], crash_handler);
+	}
+}
+
+/* vi: set ts=4 sw=4 cindent: */
