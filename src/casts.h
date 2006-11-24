1cab8348abd14770cb98a869eb6de83cdee13be2
Christian Biere <christianbiere@gmx.de>
2006-11-24 14:36:35 +0000

    * Added ALWAYS_INLINE because otherwise GCC does not inline these at -O0.

--- src/casts.h
+++ src/casts.h
@@ -42,43 +42,43 @@
  * print a diagnostic message if you accidently try to deconstify an
  * incompatible type. A direct typecast would hide such a mistake.
  */
-static inline G_GNUC_CONST WARN_UNUSED_RESULT gchar *
+static inline G_GNUC_CONST WARN_UNUSED_RESULT ALWAYS_INLINE gchar *
 deconstify_gchar(const gchar *p)
 {
 	return (gchar *) p;
 }
 
-static inline G_GNUC_CONST WARN_UNUSED_RESULT guint32 *
+static inline G_GNUC_CONST WARN_UNUSED_RESULT ALWAYS_INLINE guint32 *
 deconstify_guint32(const guint32 *p)
 {
 	return (guint32 *) p;
 }
 
-static inline G_GNUC_CONST WARN_UNUSED_RESULT gpointer
+static inline G_GNUC_CONST WARN_UNUSED_RESULT ALWAYS_INLINE gpointer
 deconstify_gpointer(gconstpointer p)
 {
 	return (gpointer) p;
 }
 
-static inline G_GNUC_CONST WARN_UNUSED_RESULT gconstpointer
+static inline G_GNUC_CONST WARN_UNUSED_RESULT ALWAYS_INLINE gconstpointer
 cast_to_gconstpointer(gconstpointer p)
 {
 	return p; 
 }
 
-static inline G_GNUC_CONST WARN_UNUSED_RESULT gpointer
+static inline G_GNUC_CONST WARN_UNUSED_RESULT ALWAYS_INLINE gpointer
 cast_to_gpointer(gpointer p)
 {
 	return p;
 }
 
-static inline G_GNUC_CONST WARN_UNUSED_RESULT gchar *
+static inline G_GNUC_CONST WARN_UNUSED_RESULT ALWAYS_INLINE gchar *
 cast_to_gchar_ptr(gpointer p)
 {
 	return p;
 }
 
-static inline G_GNUC_CONST WARN_UNUSED_RESULT guchar *
+static inline G_GNUC_CONST WARN_UNUSED_RESULT ALWAYS_INLINE guchar *
 cast_to_guchar_ptr(gpointer p)
 {
 	return p;
@@ -87,7 +87,7 @@ cast_to_guchar_ptr(gpointer p)
 /**
  * FIXME: Use uintptr_t if available.
  */
-static inline G_GNUC_CONST WARN_UNUSED_RESULT gulong
+static inline G_GNUC_CONST WARN_UNUSED_RESULT ALWAYS_INLINE gulong
 cast_ptr_to_uintptr(gpointer p)
 {
 	gulong u = (gulong) p;
@@ -98,7 +98,7 @@ cast_ptr_to_uintptr(gpointer p)
 /**
  * FIXME: Use uintptr_t if available.
  */
-static inline G_GNUC_CONST WARN_UNUSED_RESULT gpointer
+static inline G_GNUC_CONST WARN_UNUSED_RESULT ALWAYS_INLINE gpointer
 cast_uintptr_to_ptr(gulong u)
 {
 	gpointer p = (gpointer) u;
@@ -108,7 +108,7 @@ cast_uintptr_to_ptr(gulong u)
 
 typedef void (*func_ptr_t)(void);
 
-static inline G_GNUC_CONST WARN_UNUSED_RESULT gpointer
+static inline G_GNUC_CONST WARN_UNUSED_RESULT ALWAYS_INLINE gpointer
 cast_func_to_gpointer(func_ptr_t f)
 {
 	union {
@@ -120,7 +120,7 @@ cast_func_to_gpointer(func_ptr_t f)
 	return u.p;
 }
 
-static inline G_GNUC_CONST WARN_UNUSED_RESULT func_ptr_t
+static inline G_GNUC_CONST WARN_UNUSED_RESULT ALWAYS_INLINE func_ptr_t
 cast_gpointer_to_func(gconstpointer p)
 {
 	union {
@@ -132,7 +132,7 @@ cast_gpointer_to_func(gconstpointer p)
 	return u.f;
 }
 
-static inline size_t
+static inline size_t G_GNUC_CONST WARN_UNUSED_RESULT ALWAYS_INLINE
 ptr_diff(gconstpointer a, gconstpointer b)
 {
 	return (gchar *) a - (gchar *) b;
@@ -143,7 +143,7 @@ ptr_diff(gconstpointer a, gconstpointer b)
  *
  * @return On failure (off_t) -1 is returned.
  */
-static inline off_t
+static inline off_t G_GNUC_CONST WARN_UNUSED_RESULT ALWAYS_INLINE
 filesize_to_off_t(filesize_t pos)
 {
 	off_t offset = pos > OFF_T_MAX ? (off_t) -1 : (off_t) pos;
