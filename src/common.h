1f2b77e780f261463ade637cf088192e1a0865f0
Christian Biere <christianbiere@gmx.de>
2007-02-14 01:25:47 +0000

    * Bumped date.

--- src/common.h
+++ src/common.h
@@ -47,7 +47,7 @@
 #define GTA_PATCHLEVEL 4			/**< patch level or teeny version */
 #define GTA_REVISION "unstable"		/**< unstable, beta, stable */
 #define GTA_REVCHAR "u"				/**< (u)nstable, (b)eta, none -> stable */
-#define GTA_RELEASE "2007-02-07"	/**< ISO 8601 format YYYY-MM-DD */
+#define GTA_RELEASE "2007-02-14"	/**< ISO 8601 format YYYY-MM-DD */
 #define GTA_WEBSITE "http://gtk-gnutella.sourceforge.net/"
 
 #if defined(USE_GTK1)
