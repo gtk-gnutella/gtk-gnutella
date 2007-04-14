01be856d451fc072cb0fe3358124afb5110d850b
Christian Biere <christianbiere@gmx.de>
2007-04-14 08:18:26 +0000

    * Added tth_cache_file_create().  * Added tth_cache_check().  * tth_cache_lookup(): Return the number of nodes instead of the depth.

--- src/core/tth_cache.c
+++ src/core/tth_cache.c
@@ -45,11 +45,19 @@ RCSID("$Id$")
 #include "settings.h"
 #include "tth_cache.h"
 
+#include "lib/atoms.h"
 #include "lib/file.h"
 #include "lib/misc.h"
+#include "lib/tigertree.h"
 #include "lib/walloc.h"
 
 #include "lib/override.h"       /* Must be the last header included */
+
+#if defined(S_IROTH)
+#define TTH_FILE_MODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH) /* 0644 */
+#else
+#define TTH_FILE_MODE (S_IRUSR | S_IWUSR | S_IRGRP) /* 0640 */
+#endif
 
 static const char *
 tth_cache_directory(void)
@@ -70,7 +78,28 @@ tth_cache_pathname(const struct tth *tth)
 }
 
 static int
-tth_cache_file_open(const struct tth *tth, int accmode)
+tth_cache_file_create(const struct tth *tth)
+{
+	char *pathname;
+	int accmode;
+	int fd;
+
+	g_return_val_if_fail(tth, -1);
+
+	accmode = O_WRONLY | O_TRUNC;
+	pathname = tth_cache_pathname(tth);
+	fd = file_create(pathname, accmode, TTH_FILE_MODE);
+	if (fd < 0 && ENOENT == errno) {
+		if (0 == create_directory(tth_cache_directory())) {
+			fd = file_create(pathname, accmode, TTH_FILE_MODE);
+		}
+	}
+	G_FREE_NULL(pathname);
+	return fd;
+}
+
+static int
+tth_cache_file_open(const struct tth *tth)
 {
 	char *pathname;
 	int fd;
@@ -78,7 +107,7 @@ tth_cache_file_open(const struct tth *tth, int accmode)
 	g_return_val_if_fail(tth, -1);
 
 	pathname = tth_cache_pathname(tth);
-	fd = file_open(pathname, accmode);
+	fd = file_open(pathname, O_RDONLY);
 	G_FREE_NULL(pathname);
 	return fd;
 }
@@ -92,7 +121,7 @@ tth_cache_insert(const struct tth *tth, const struct tth *leaves, int n)
 	g_return_if_fail(leaves);
 	g_return_if_fail(n >= 1);
 
-	fd = tth_cache_file_open(tth, O_WRONLY);
+	fd = tth_cache_file_create(tth);
 	if (fd >= 0) {
 		struct iovec *iov;
 		ssize_t ret;
@@ -119,46 +148,79 @@ tth_cache_insert(const struct tth *tth, const struct tth *leaves, int n)
 	}
 }
 
+static gboolean
+tth_cache_check(const struct tth *tth, int fd, filesize_t filesize)
+{
+	struct tth nodes[1 << (TTH_MAX_DEPTH - 1)];
+	char buf[G_N_ELEMENTS(nodes) * TTH_RAW_SIZE];
+	size_t n_nodes, i, size;
+	ssize_t r;
+
+	g_return_val_if_fail(tth, FALSE);
+	g_return_val_if_fail(fd >= 0, FALSE);
+	g_return_val_if_fail(filesize >= TTH_RAW_SIZE, FALSE);
+	g_return_val_if_fail(0 == (filesize % TTH_RAW_SIZE), FALSE);
+
+	n_nodes = filesize / TTH_RAW_SIZE;
+	g_return_val_if_fail(n_nodes <= G_N_ELEMENTS(nodes), FALSE);
+
+	size = n_nodes * TTH_RAW_SIZE;
+	r = read(fd, buf, size);
+	g_return_val_if_fail((size_t) r == size, FALSE);
+
+	for (i = 0; i < n_nodes; i++) {
+		memmove(nodes[i].data, &buf[i * TTH_RAW_SIZE], TTH_RAW_SIZE);
+	}
+	return tth_eq(tth, tt_root_hash(nodes, n_nodes));
+}
+
 /**
- * @return The depth of the cached tigertree or a non-positive value
- *		   if there's none.
+ * @return The number of leaves or zero if unknown.
  */
-int
+size_t
 tth_cache_lookup(const struct tth *tth)
 {
-	int fd, depth, result = 0;
+	int fd, depth;
+	filesize_t size;
+	size_t result;
 	struct stat sb;
 	
 	g_return_val_if_fail(tth, 0);
 
-	fd = tth_cache_file_open(tth, O_RDONLY);
+	fd = tth_cache_file_open(tth);
 	if (fd < 0) {
 		goto finish;
 	}
 	if (fstat(fd, &sb)) {
-		g_warning("tth_cache_lookup(): fstat() failed for \"%s\": %s",
-			tth_base32(tth), g_strerror(errno));
+		g_warning("tth_cache_lookup(%s): fstat() failed: %s", tth_base32(tth),
+			g_strerror(errno));
 		goto finish;
 	}
 	if (!S_ISREG(sb.st_mode)) {
-		g_warning("tth_cache_lookup(): Not a regular file \"%s\"",
-			tth_base32(tth));
+		g_warning("tth_cache_lookup(%s): Not a regular file", tth_base32(tth));
 		goto finish;
 	}
 	if (sb.st_size < TTH_RAW_SIZE || sb.st_size % TTH_RAW_SIZE) {
-		g_warning("tth_cache_lookup(): Bad filesize \"%s\"",
-			tth_base32(tth));
+		g_warning("tth_cache_lookup(%s): Bad filesize %s",
+			tth_base32(tth), off_t_to_string(sb.st_size));
 		goto finish;
 	}
 
+	size = sb.st_size / TTH_RAW_SIZE;
 	depth = 1;
-	sb.st_size /= TTH_RAW_SIZE;
-	while (sb.st_size > 1) {
-		sb.st_size = (sb.st_size + 1) / 2;
+	while (size > 1) {
+		size = (size + 1) / 2;
 		depth++;
 	}
-	
-	result = depth;
+	if (depth > TTH_MAX_DEPTH) {
+		g_warning("tth_cache_lookup(%s): Bad depth %u", tth_base32(tth), depth);
+		goto finish;
+	}
+	if (tth_cache_check(tth, fd, sb.st_size)) {
+		g_warning("tth_cache_lookup(%s): Damaged file", tth_base32(tth));
+		goto finish;
+	}
+	result = sb.st_size / TTH_RAW_SIZE;
 	
 finish:
 
