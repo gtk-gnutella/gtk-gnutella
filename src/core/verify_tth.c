62c192cd33c6f653236ceb413ce46627ee78e81f
Christian Biere <christianbiere@gmx.de>
2007-04-14 08:22:37 +0000

    * tigertree_step_compute(): Splitted into multiple functions for readability.  * Store the calculated tigertree root hash and the leaves.

--- src/core/verify_tth.c
+++ src/core/verify_tth.c
@@ -43,9 +43,13 @@ RCSID("$Id$")
 #include "downloads.h"
 #include "file_object.h"
 #include "guid.h"
+#include "huge.h"
 #include "sockets.h"
 #include "tth_cache.h"
 #include "verify_tth.h"
+
+#include "if/gnet_property.h"
+#include "if/gnet_property_priv.h"
 
 #include "lib/atoms.h"
 #include "lib/base32.h"
@@ -72,6 +76,7 @@ struct verify_tth {
 	size_t buffer_size;
 	char *buffer;
 	TTH_CONTEXT *tth;
+	struct shared_file *sf;
 };
 
 /**
@@ -129,6 +134,7 @@ verify_tth_free(struct verify_tth **ptr)
 
 	if (ctx) {
 		file_object_release(&ctx->file);
+		shared_file_unref(&ctx->sf);
 		G_FREE_NULL(ctx->buffer);
 		wfree(ctx->tth, tt_size());
 		wfree(ctx, sizeof *ctx);
@@ -145,114 +151,161 @@ verify_tth_context_free(void *data)
 	verify_tth_free(&ctx);
 }
 
+static void
+tigertree_next_file(struct verify_tth *ctx)
+{
+	gboolean success = FALSE;
+
+	g_assert(ctx);
+	g_assert(NULL == ctx->sf);
+	g_assert(NULL == ctx->file);
+	
+	ctx->sf = files_to_hash ? slist_shift(files_to_hash) : NULL;
+	if (ctx->sf) {
+		const char *pathname;
+
+		pathname = shared_file_path(ctx->sf);
+		ctx->file = file_object_open(pathname, O_RDONLY);
+		if (NULL == ctx->file) {
+			int fd;
+
+			fd = file_open(pathname, O_RDONLY);
+			if (fd >= 0) {
+				ctx->file = file_object_new(fd, pathname, O_RDONLY);
+			}
+		}
+		if (NULL == ctx->file) {
+			g_warning("Failed to open \"%s\" for tigertree hashing: %s",
+					pathname, g_strerror(errno));
+		}
+	}
+
+	if (ctx->file) {
+		struct stat sb;
+
+		if (fstat(file_object_get_fd(ctx->file), &sb)) {
+			g_warning("fstat() failed for \"%s\": %s",
+				file_object_get_pathname(ctx->file), g_strerror(errno));
+		} else if (!S_ISREG(sb.st_mode) || sb.st_size < 0) {
+			g_warning("Not a regular file \"%s\": %s",
+				file_object_get_pathname(ctx->file), g_strerror(errno));
+		} else {
+			success = TRUE;
+			ctx->filesize = sb.st_size;
+			ctx->offset = 0;
+			tt_init(ctx->tth, ctx->filesize);
+			g_message("Starting tigertree hashing of \"%s\"",
+					file_object_get_pathname(ctx->file));
+		}
+	}
+
+	if (!success) {
+		file_object_release(&ctx->file);
+		shared_file_unref(&ctx->sf);
+	}
+}
+
+static void
+tigertree_feed(struct verify_tth *ctx)
+{
+	gboolean finished = FALSE;
+	filesize_t size;
+	ssize_t r;
+
+	g_assert(ctx);
+	g_assert(ctx->sf);
+	g_assert(ctx->file);
+
+	g_assert(ctx->filesize >= ctx->offset);
+	g_assert(ctx->buffer_size > 0);
+
+	size = ctx->filesize - ctx->offset;
+	size = MIN(size, ctx->buffer_size);
+
+	if (size > 0) {
+		r = file_object_pread(ctx->file, ctx->buffer, size, ctx->offset);
+	} else {
+		r = 0;
+	}
+	if ((ssize_t) -1 == r) {
+		if (!is_temporary_error(errno)) {
+			finished = TRUE;
+			g_warning("Error while reading file: %s", g_strerror(errno));
+		}
+	} else if (0 == r) {
+		finished = TRUE;
+		if (ctx->offset != ctx->filesize) {
+			g_warning("File shrunk");
+		}
+	} else {
+		size_t n = r;
+
+		ctx->offset += n;
+		tt_update(ctx->tth, ctx->buffer, n);
+	}
+	if (ctx->offset == ctx->filesize) {
+		struct tth tth;
+
+		finished = TRUE;
+		tt_digest(ctx->tth, &tth);
+		g_warning("File \"%s\" has TTH: %s",
+			file_object_get_pathname(ctx->file), tth_base32(&tth));
+		shared_file_set_tth(ctx->sf, &tth);
+		tth_cache_insert(&tth, tt_leaves(ctx->tth), tt_leave_count(ctx->tth));
+		huge_update_hashes(ctx->sf, shared_file_sha1(ctx->sf), &tth);
+	}
+	if (finished) {
+		file_object_release(&ctx->file);
+		shared_file_unref(&ctx->sf);
+	}
+}
+
 static bgret_t
-tigertree_step_compute(struct bgtask *h, void *data, int ticks)
+tigertree_step_compute(struct bgtask *bt, void *data, int ticks)
 {
 	struct verify_tth *ctx = data;
 
 	g_assert(ctx);
 	(void) ticks;
 
-	bg_task_ticks_used(h, 0);
+	bg_task_ticks_used(bt, 0);
 
 	if (NULL == ctx->file) {
-		struct shared_file *sf;
-
-		sf = files_to_hash ? slist_shift(files_to_hash) : NULL;
-		if (sf) {
-			const char *pathname;
-
-			pathname = shared_file_path(sf);
-			ctx->file = file_object_open(pathname, O_RDONLY);
-			if (NULL == ctx->file) {
-				int fd;
-
-				fd = file_open(pathname, O_RDONLY);
-				if (fd >= 0) {
-					ctx->file = file_object_new(fd, pathname, O_RDONLY);
-				}
-			}
-			if (ctx->file) {
-				g_warning("Starting to tigertree hashing of \"%s\"", pathname);
-			} else {
-				g_warning("Failed to open \"%s\" for tigertree hashing: %s",
-						pathname, g_strerror(errno));
-			}
-			shared_file_unref(&sf);
-		} else {
-			goto done;
-		}
-		
-		if (ctx->file) {
-			struct stat sb;
-
-			if (fstat(file_object_get_fd(ctx->file), &sb)) {
-				g_warning("fstat() failed for \"%s\": %s",
-					file_object_get_pathname(ctx->file), g_strerror(errno));
-				goto error;
-			}
-			if (!S_ISREG(sb.st_mode) || sb.st_size < 0) {
-				g_warning("Not a regular file \"%s\": %s",
-					file_object_get_pathname(ctx->file), g_strerror(errno));
-				goto error;
-			}
-			ctx->filesize = sb.st_size;
-			ctx->offset = 0;
-			tt_init(ctx->tth, ctx->filesize);
-		}
+		tigertree_next_file(ctx);
 	}
 	if (ctx->file) {
-		ssize_t r;
-
-		r = file_object_pread(ctx->file,
-				ctx->buffer, ctx->buffer_size, ctx->offset);
-		if ((ssize_t) -1 == r) {
-			if (!is_temporary_error(errno)) {
-				g_warning("Error while reading file: %s", g_strerror(errno));
-				goto error;
-			}
-		} else if (0 == r) {
-			if (ctx->offset != ctx->filesize) {
-				g_warning("File shrunk");
-				goto error;
-			} else {
-				struct tth tth;
-
-				tt_digest(ctx->tth, &tth);
-				g_warning("File \"%s\" has TTH: %s",
-					file_object_get_pathname(ctx->file), tth_base32(&tth));
-				file_object_release(&ctx->file);
-				tth_cache_insert(&tth,
-					tt_leaves(ctx->tth), tt_leave_count(ctx->tth));
-			}
-		} else {
-			size_t n = r;
-			
-			if (n > ctx->filesize - ctx->offset) {
-				g_warning("File grew");
-				goto error;
-			}
-			ctx->offset += n;
-			tt_update(ctx->tth, ctx->buffer, n);
-		}
+		tigertree_feed(ctx);
 	}
-	return BGR_MORE;
-
-error:
-	file_object_release(&ctx->file);
-	return BGR_MORE;
-
-done:
-	return BGR_DONE;
+	return ctx->file || slist_length(files_to_hash) > 0 ? BGR_MORE : BGR_DONE;
 }
 
 void
 request_tigertree(struct shared_file *sf)
 {
+	if (!experimental_tigertree_support)
+		return;
+
 	tt_verify_init();
 
 	g_return_if_fail(sf);
 	g_return_if_fail(files_to_hash);
+
+	if (shared_file_tth(sf)) {
+		const struct tth *tth;
+		size_t n, ret;
+		
+		tth = shared_file_tth(sf);
+		n = tt_bottom_node_count(shared_file_size(sf));
+		ret = tth_cache_lookup(tth);
+		if (n == ret) {
+			g_message("TTH %s is already cached", tth_base32(tth));
+		} else {
+			g_message("TTH %s has %lu/%lu nodes",
+				tth_base32(tth), (gulong) ret, (gulong) n);
+			huge_update_hashes(sf, shared_file_sha1(sf), NULL);
+		}
+		return;
+	}
 
 	slist_append(files_to_hash, shared_file_ref(sf));
 
