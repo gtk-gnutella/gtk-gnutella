01125d6848da6c6c656fce7626369246fe661acd
Christian Biere <christianbiere@gmx.de>
2007-04-21 10:28:12 +0000

    * Added thex.h for definitions shared by thex_upload.c and thex_download.c.

---
+++ src/core/thex.h
@@ -0,0 +1,15 @@
+
+#ifndef _core_thex_h_
+#define _core_thex_h_
+
+#include "common.h"
+
+#define THEX_DOCTYPE		"http://open-content.net/spec/thex/thex.dtd"
+#define THEX_TREE_TYPE		"http://open-content.net/spec/thex/breadthfirst"
+#define THEX_HASH_ALGO		"http://open-content.net/spec/digest/tiger"
+#define THEX_HASH_SIZE		"24"	/* TTH_RAW_SIZE */
+#define THEX_SEGMENT_SIZE	"1024"	/* TTH_BLOCKSIZE */
+
+#endif /* _core_thex_upload_h_ */
+
+/* vi: set ts=4 sw=4 cindent: */
