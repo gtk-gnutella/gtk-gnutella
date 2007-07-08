ad5189113641e9da2914e677527eb7ccef8b6bf6
Christian Biere <christianbiere@gmx.de>
2007-07-08 12:45:28 +0000

    * Back to the future.

--- src/common.h
+++ src/common.h
@@ -44,10 +44,10 @@
 
 #define GTA_VERSION 0				/**< major version */
 #define GTA_SUBVERSION 96			/**< minor version */
-#define GTA_PATCHLEVEL 4			/**< patch level or teeny version */
-#define GTA_REVISION "stable"		/**< unstable, beta, stable */
-#define GTA_REVCHAR ""				/**< (u)nstable, (b)eta, none -> stable */
-#define GTA_RELEASE "2007-07-07"	/**< ISO 8601 format YYYY-MM-DD */
+#define GTA_PATCHLEVEL 5			/**< patch level or teeny version */
+#define GTA_REVISION "unstable"		/**< unstable, beta, stable */
+#define GTA_REVCHAR "u"				/**< (u)nstable, (b)eta, none -> stable */
+#define GTA_RELEASE "2007-07-08"	/**< ISO 8601 format YYYY-MM-DD */
 #define GTA_WEBSITE "http://gtk-gnutella.sourceforge.net/"
 
 #if defined(USE_GTK1)
