446d72d6e977d43169c104ca84c2189b64cff5ae
Christian Biere <christianbiere@gmx.de>
2007-09-11 01:44:33 +0000

    * socket_addr_getsockname(), socket_accept(): Use socket_addr_init() to    clear and prepare the address structure. Maybe this makes Darwin happy.

--- src/core/sockets.c
+++ src/core/sockets.c
@@ -1971,43 +1971,32 @@ socket_addr_getsockname(socket_addr_t *p_addr, int fd)
 static int
 socket_addr_getpeername(socket_addr_t *p_addr, int fd)
 {
-	struct sockaddr_in sin4;
-	socklen_t len;
-	host_addr_t addr = zero_host_addr;
-	guint16 port = 0;
-	int error = 0;
+	socklen_t addr_len;
 
-	len = sizeof sin4;
-	memset(&sin4, 0, sizeof sin4);
-	sin4.sin_family = AF_INET;
-	if (-1 != getpeername(fd, cast_to_gpointer(&sin4), &len)) {
-		addr = host_addr_peek_ipv4(&sin4.sin_addr.s_addr);
-		port = sin4.sin_port;
-		error = errno;
+	g_return_val_if_fail(p_addr, -1);
+	g_return_val_if_fail(fd >= 0, -1);
+
+	errno = 0;
+	addr_len = socket_addr_init(p_addr, NET_TYPE_IPV4);
+	if (
+		-1 != getpeername(fd, socket_addr_get_sockaddr(p_addr), &addr_len) &&
+		AF_INET == socket_addr_get_family(p_addr)
+	) {
+		return 0;
 	}
 
 #ifdef HAS_IPV6
-	if (!is_host_addr(addr)) {
-		struct sockaddr_in6 sin6;
-
-		len = sizeof sin6;
-		memset(&sin6, 0, sizeof sin6);
-		sin6.sin6_family = AF_INET6;
-		if (-1 != getpeername(fd, cast_to_gpointer(&sin6), &len)) {
-			addr = host_addr_peek_ipv6(sin6.sin6_addr.s6_addr);
-			port = sin6.sin6_port;
-			error = errno;
-		}
+	errno = 0;
+	addr_len = socket_addr_init(p_addr, NET_TYPE_IPV6);
+	if (
+		-1 != getpeername(fd, socket_addr_get_sockaddr(p_addr), &addr_len) &&
+		AF_INET6 == socket_addr_get_family(p_addr)
+	) {
+		return 0;
 	}
 #endif	/* HAS_IPV6 */
 
-	if (!is_host_addr(addr)) {
-		errno = error;	/* Clear it if getpeername() did not fail */
-		return -1;
-	}
-	
-	socket_addr_set(p_addr, addr, port);
-	return 0;
+	return -1;
 }
 
 /**
@@ -2085,9 +2074,8 @@ socket_accept(gpointer data, gint unused_source, inputevt_cond_t cond)
 		return;
 	}
 
-	addr_len = sizeof addr;
-	memset(&addr, 0, addr_len);
-	fd = accept(s->file_desc, (struct sockaddr *) &addr, &addr_len);
+	addr_len = socket_addr_init(&addr, s->net);
+	fd = accept(s->file_desc, socket_addr_get_sockaddr(&addr), &addr_len);
 	if (fd < 0) {
 		/*
 		 * If we ran out of file descriptors, try to reclaim one from the
@@ -2098,9 +2086,9 @@ socket_accept(gpointer data, gint unused_source, inputevt_cond_t cond)
 			(errno == EMFILE || errno == ENFILE) &&
 			reclaim_fd != NULL && (*reclaim_fd)()
 		) {
-			addr_len = sizeof addr;
-			memset(&addr, 0, addr_len);
-			fd = accept(s->file_desc, (struct sockaddr *) &addr, &addr_len);
+			addr_len = socket_addr_init(&addr, s->net);
+			fd = accept(s->file_desc, socket_addr_get_sockaddr(&addr),
+					&addr_len);
 		}
 
 		if (fd < 0) {
