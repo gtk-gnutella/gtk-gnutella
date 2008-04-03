53ab7d41203a5a26ee11d9b365dca52f607e8e0c
Christian Biere <christianbiere@gmx.de>
2008-04-03 17:32:46 +0000

    * This is no longer stable.

--- src/common.h
+++ src/common.h
@@ -45,10 +45,10 @@
 #define GTA_PRODUCT_NAME "gtk-gnutella"	/**< Normally "gtk-gnutella" */
 #define GTA_VERSION 0				/**< major version */
 #define GTA_SUBVERSION 96			/**< minor version */
-#define GTA_PATCHLEVEL 5			/**< patch level or teeny version */
-#define GTA_REVISION "stable"		/**< unstable, beta, stable */
-#define GTA_REVCHAR ""				/**< (u)nstable, (b)eta, none -> stable */
-#define GTA_RELEASE "2008-04-01"	/**< ISO 8601 format YYYY-MM-DD */
+#define GTA_PATCHLEVEL 6			/**< patch level or teeny version */
+#define GTA_REVISION "unstable"		/**< unstable, beta, stable */
+#define GTA_REVCHAR "u"				/**< (u)nstable, (b)eta, none -> stable */
+#define GTA_RELEASE "2008-04-03"	/**< ISO 8601 format YYYY-MM-DD */
 #define GTA_WEBSITE "http://gtk-gnutella.sourceforge.net/"
 
 #if defined(USE_GTK1)
