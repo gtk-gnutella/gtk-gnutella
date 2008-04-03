cd0d4cb81e255802e61042a5a28166b9b8f24866
Christian Biere <christianbiere@gmx.de>
2008-04-03 17:31:14 +0000

    * Fix for issue #1932909 "columns in search window do not scale properly".

--- src/ui/gtk/gtk2/search.c
+++ src/ui/gtk/gtk2/search.c
@@ -353,7 +353,6 @@ add_column(
 		"reorderable", FALSE,
 		"resizable", TRUE,
 		"sizing", GTK_TREE_VIEW_COLUMN_FIXED,
-		"expand", (gboolean) (0 == id),
 		(void *) 0);
 	
     gtk_tree_view_append_column(tv, column);
