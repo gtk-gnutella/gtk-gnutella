f7ee6f244e9d4adc8cb861c8a6c3f65c3a7dfff8
Christian Biere <christianbiere@gmx.de>
2009-04-23 17:21:53 +0000

    * Fixed signed vs. unsigned comparison warning.

--- src/core/sq.c
+++ src/core/sq.c
@@ -415,7 +415,7 @@ retry:
 		if (GNET_PROPERTY(current_peermode) != NODE_P_ULTRA)
 			return;
 
-		if (node_keep_missing() * 3 > 2 * GNET_PROPERTY(up_connections))
+		if (UNSIGNED(node_keep_missing()) * 3 > 2 * GNET_PROPERTY(up_connections))
 			return;							/* Not enough nodes for querying */
 	}
 
