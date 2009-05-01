99b55f7f34ec0659e2ca2cfeca63cdfea3fbc534
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2009-05-01 19:46:53 +0000

    Propagated enhancements made to chkpage() into okpage().

--- src/sdbm/util.c
+++ src/sdbm/util.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <errno.h>
+#include "common.h"
 #ifdef SDBM
 #include "sdbm.h"
 #else
@@ -8,15 +10,12 @@
 void
 oops(register char *s1, register char *s2)
 {
-	extern int errno, sys_nerr;
-	extern char *sys_errlist[];
 	extern char *progname;
 
 	if (progname)
 		fprintf(stderr, "%s: ", progname);
 	fprintf(stderr, s1, s2);
-	if (errno > 0 && errno < sys_nerr)
-		fprintf(stderr, " (%s)", sys_errlist[errno]);
+	fprintf(stderr, " (%s)", strerror(errno));
 	fprintf(stderr, "\n");
 	exit(1);
 }
@@ -25,19 +24,26 @@ int
 okpage(char *pag)
 {
 	register unsigned n;
-	register off;
+	register int off;
 	register short *ino = (short *) pag;
+	int ino_end;
 
-	if ((n = ino[0]) > PBLKSIZ / sizeof(short))
+	if ((n = ino[0]) > DBM_PBLKSIZ / sizeof(short))
 		return 0;
 
 	if (!n)
 		return 1;
 
-	off = PBLKSIZ;
+	if (n & 0x1)
+		return 0;
+
+	ino_end = (n + 1) * sizeof(short);
+
+	off = DBM_PBLKSIZ;
 	for (ino++; n; ino += 2) {
-		if (ino[0] > off || ino[1] > off ||
-		    ino[1] > ino[0])
+		if (ino[0] > off || ino[1] > off || ino[1] > ino[0])
+			return 0;
+		if (ino[0] < ino_end || ino[1] < ino_end)
 			return 0;
 		off = ino[1];
 		n -= 2;
