dc63a046dc76d2e9fb1cbd3c067b2f2a505313d0
Christian Biere <christianbiere@gmx.de>
2009-06-06 20:50:09 +0000

    * Removed unnecessary check.

--- src/ui/gtk/search_common.c
+++ src/ui/gtk/search_common.c
@@ -1629,10 +1629,7 @@ search_gui_color_for_record(const record_t * const rc)
 		return GUI_COLOR_SPAM;
 	} else if (ST_HOSTILE & rs->status) {
 		return GUI_COLOR_HOSTILE;
-	} else if (
-		(SR_SPAM & rc->flags) ||
-		((ST_SPAM & rs->status) && !(ST_BROWSE & rs->status))
-	) {
+	} else if ((ST_SPAM & rs->status) && !(ST_BROWSE & rs->status)) {
 		return GUI_COLOR_MAYBE_SPAM;
 	} else if (rs->status & ST_UNREQUESTED) {
 		return GUI_COLOR_UNREQUESTED;
