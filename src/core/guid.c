040b5271cb959d4e9cbc4656bbe9e83038c42366
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2009-11-12 00:20:31 +0000

    Another printf -> g_message conversion.

--- src/core/guid.c
+++ src/core/guid.c
@@ -175,7 +175,7 @@ guid_init(void)
 		guid_gtkg_encode_version(GTA_VERSION, GTA_SUBVERSION, *rev == '\0');
 
 	if (GNET_PROPERTY(node_debug))
-		printf("GTKG version mark is 0x%x\n", gtkg_version_mark);
+		g_message("GTKG version mark is 0x%x", gtkg_version_mark);
 
 	/*
 	 * Validate that guid_random_muid() correctly marks GUIDs as being GTKG's.
