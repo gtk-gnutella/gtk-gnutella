6d28b55b9dd683f03cebac07e19f655b09ed60c1
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2010-11-18 23:01:45 +0000

    lookup_handle_reply(): ignored unsafe hosts in contacts logged at level 2.

--- src/dht/lookup.c
+++ src/dht/lookup.c
@@ -2798,7 +2798,7 @@ lookup_handle_reply(
 	knode_check(kn);
 
 	unsafe_len = GNET_PROPERTY(dht_lookup_debug) ? sizeof unsafe : 0;
-	msg[0] = '\0';
+	unsafe[0] = msg[0] = '\0';
 
 	if (GNET_PROPERTY(dht_lookup_debug) > 2) {
 		tm_t now;
@@ -3050,7 +3050,7 @@ lookup_handle_reply(
 		continue;
 
 	skip:
-		if (GNET_PROPERTY(dht_lookup_debug) > 4)
+		if (GNET_PROPERTY(dht_lookup_debug) > unsafe[0] ? 1 : 4)
 			g_debug("DHT LOOKUP[%s] ignoring %scontact #%d: %s",
 				revent_id_to_string(nl->lid),
 				map_contains(nl->fixed, cn->id) ? "(fixed) " : "",
