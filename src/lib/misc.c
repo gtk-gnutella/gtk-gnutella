df4cb7eef74f9e915bd73ab34b74f8ce1a4828fe
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2010-12-31 23:00:27 +0000

    is_strcaseprefix(): no need to compare a and b, only compare the output of ascii_tolower() since that routine acts as the identity function for non upper-cased characters.

--- src/lib/misc.c
+++ src/lib/misc.c
@@ -116,7 +116,7 @@ is_strcaseprefix(const char *str, const char *prefix)
 
 	for (s = str, p = prefix; '\0' != (a = *p); p++) {
 		int b = *s++;
-		if (a != b && ascii_tolower(a) != ascii_tolower(b))
+		if (ascii_tolower(a) != ascii_tolower(b))
 			return NULL;
 	}
 
