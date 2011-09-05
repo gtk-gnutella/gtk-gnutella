7427aabf34154f3491862de03a4b632c9145e089
Jeroen Asselman <jeroen@MacBook-Pro-Werk.local>
2011-09-05 20:43:41 +0200

    Adds version git script

---
+++ scripts/git-version.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+DEF_VER=v0.97
+
+LF='
+'
+
+# First see if there is a version file (included in release tarballs),
+# then try git-describe, then default.
+if test -f version
+then
+	VN=$(cat version) || VN="$DEF_VER"
+elif test -d .git -o -f .git &&
+	VN=$(git describe --match "v[0-9]*" --abbrev=4 HEAD 2>/dev/null) &&
+	case "$VN" in
+	*$LF*) (exit 1) ;;
+	v[0-9]*)
+		git update-index -q --refresh
+		test -z "$(git diff-index --name-only HEAD --)" ||
+		VN="$VN-dirty" ;;
+	esac
+then
+	VN="$VN"
+else
+	VN="$DEF_VER"
+fi
+
+VN=$(expr "$VN" : v*'\(.*\)')
+
+echo $VN
+V=$VN
+
+VMajor=${V%%.*}
+V=${V:${#VMajor}}
+
+[ "${V:0:1}" == "." ] && V=${V:1} && VMinor=${V%%[.-]*}
+V=${V:${#VMinor}}
+
+[ "${V:0:1}" == "." ] && V=${V:1} && VPatch=${V%%-*} && V=${V:${#VPatch}} || VPatch=0
+
+VRev=${V:1}
+
