e0e72d2470834a83887a8f231d70030040611658
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2011-10-31 09:17:10 +0100

    node_eof_v(): emit error message after EOF-style errors during BYE on one line.

--- src/core/nodes.c
+++ src/core/nodes.c
@@ -2493,18 +2493,17 @@ node_eof_v(struct gnutella_node *n, const char *reason, va_list args)
 	if (n->flags & NODE_F_BYE_SENT) {
 		g_assert(n->status == GTA_NODE_SHUTDOWN);
 		if (GNET_PROPERTY(node_debug)) {
-			g_debug("EOF-style error during BYE to %s:\n (BYE) ",
-				node_addr(n));
+			str_t str;
+			char data[128];
+			va_list dbargs;
 
-			if (log_printable(LOG_STDERR)) {
-				va_list dbargs;
+			str_from_foreign(&str, data, 0, sizeof data);
+			VA_COPY(dbargs, args);
+			str_vprintf(&str, reason, dbargs);
+			va_end(dbargs);
 
-				VA_COPY(dbargs, args);
-				vfprintf(stderr, reason, dbargs);
-				va_end(dbargs);
-
-				fprintf(stderr, "\n");
-			}
+			g_debug("EOF-style error during BYE to %s: %s",
+				node_addr(n), str_2c(&str));
 		}
 	}
 
