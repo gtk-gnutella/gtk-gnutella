e9da362ad66115558dbff0fbf620a76262003d1d
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2011-11-01 20:10:24 +0100

    Added the "command" shell command to print the server's command line.

---
+++ src/shell/command.c
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2011, Raphael Manfredi
+ *
+ *----------------------------------------------------------------------
+ * This file is part of gtk-gnutella.
+ *
+ *  gtk-gnutella is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  gtk-gnutella is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with gtk-gnutella; if not, write to the Free Software
+ *  Foundation, Inc.:
+ *      59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *----------------------------------------------------------------------
+ */
+
+/**
+ * @ingroup shell
+ * @file
+ *
+ * The "command" command.
+ *
+ * @author Raphael Manfredi
+ * @date 2011
+ */
+
+#include "common.h"
+
+#include "cmd.h"
+
+#include "if/core/main.h"
+#include "lib/halloc.h"
+
+#include "lib/override.h"		/* Must be the last header included */
+
+/**
+ * Print command line used to launch this process.
+ */
+enum shell_reply
+shell_exec_command(struct gnutella_shell *sh, int argc, const char *argv[])
+{
+	char *cmd;
+
+	shell_check(sh);
+	g_assert(argv);
+	g_assert(argc > 0);
+
+	cmd = main_command_line();
+	shell_write_line(sh, REPLY_READY, cmd);
+	HFREE_NULL(cmd);
+
+	return REPLY_READY;
+}
+
+const char *
+shell_summary_command(void)
+{
+	return "Shows process command line";
+}
+
+const char *
+shell_help_command(int argc, const char *argv[])
+{
+	g_assert(argv);
+	g_assert(argc > 0);
+
+	return "Prints the full command line used to launch the server.\n";
+}
+
+/* vi: set ts=4 sw=4 cindent: */
