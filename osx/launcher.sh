92fd34cec288aa08d231ab3b0d6e4b79b8b24086
Jeroen Asselman <jeroen@asselman.com>
2011-11-09 19:48:57 +0100

    APP variable should be set to gtk-gnutella

--- osx/launcher.sh
+++ osx/launcher.sh
@@ -34,7 +34,7 @@ export GTK_IM_MODULE_FILE="$bundle_etc/gtk-2.0/gtk.immodules"
 export GDK_PIXBUF_MODULE_FILE="$bundle_etc/gtk-2.0/gdk-pixbuf.loaders"
 export PANGO_RC_FILE="$bundle_etc/pango/pangorc"
 
-APP=name
+APP=gtk-gnutella
 I18NDIR="$bundle_data/locale"
 # Set the locale-related variables appropriately:
 unset LANG LC_MESSAGES LC_MONETARY LC_COLLATE
