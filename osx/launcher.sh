a425f505c4e778d87ade718b6691e39e0313c832
Jeroen Asselman <jeroen@asselman.com>
2011-11-16 07:26:41 +0100

    Raise the default number of allowed fds to 2048 on OS X

--- osx/launcher.sh
+++ osx/launcher.sh
@@ -169,5 +169,8 @@ cd "$bundle_contents/Resources/"
 
 export NLSPATH="$I18NDIR"
 
+# Number of allowed file descriptors
+ulimit -n 2048
+
 $EXEC "$bundle_contents/MacOS/$name-bin" $* $EXTRA_ARGS
 #$EXEC "$bundle_res/gtk-gnutella" $* $EXTRA_ARGS
