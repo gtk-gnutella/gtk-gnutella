829315d9ab1d043d62b469fb7c9f857c8320d82a
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2011-11-18 01:25:54 +0100

    Enhanced sdbm/dbe.
    Added ability to load values / dump values to a file.
    Added ability to display keys and values in hexadecimal.
    Added ability to dump values as-is without any transformation.
    Added ability to display only the keys.
    Added strerror() output on errors.
    
    Fixed regexec() call: returns 0 on matches, contrary to old re_exec() call.

--- src/sdbm/dbe.c
+++ src/sdbm/dbe.c
@@ -1,5 +1,6 @@
 #include "common.h"
 #include "sdbm.h"
+#include "lib/base16.h"
 
 extern G_GNUC_PRINTF(1, 2) void oops(char *fmt, ...);
 
@@ -123,68 +124,151 @@ my_getopt(int argc, char **argv, char *optstring)
 
 
 void
-print_datum(datum db)
+print_datum(FILE *f, datum db, int hexa)
 {
 	size_t i;
 
-	putchar('"');
-	for (i = 0; i < db.dsize; i++) {
-		if (isprint((unsigned char)db.dptr[i]))
-			putchar(db.dptr[i]);
-		else {
-			putchar('\\');
-			putchar('0' + ((db.dptr[i] >> 6) & 0x07));
-			putchar('0' + ((db.dptr[i] >> 3) & 0x07));
-			putchar('0' + (db.dptr[i] & 0x07));
+	if (hexa > 0) {
+		const char *hex = "0123456789abcdef";
+		for (i = 0; i < db.dsize; i++) {
+			unsigned v = (unsigned char) db.dptr[i];
+			fputc(hex[v >> 4], f);
+			fputc(hex[v & 0xf], f);
 		}
+	} else if (hexa < 0) {
+		for (i = 0; i < db.dsize; i++) {
+			fputc(db.dptr[i], f);
+		}
+	} else {
+		fputc('"', f);
+		for (i = 0; i < db.dsize; i++) {
+			if (isprint((unsigned char)db.dptr[i]))
+				fputc(db.dptr[i], f);
+			else {
+				fputc('\\', f);
+				fputc('0' + ((db.dptr[i] >> 6) & 0x07), f);
+				fputc('0' + ((db.dptr[i] >> 3) & 0x07), f);
+				fputc('0' + (db.dptr[i] & 0x07), f);
+			}
+		}
+		fputc('"', f);
 	}
-	putchar('"');
 }
 
 
 datum
-read_datum(char *s)
+read_datum(char *s, int hexa, const char *what)
 {
 	datum db;
-	char *p;
-	int i;
+	size_t len, dest;
 
+	dest = len = strlen(s);
+	if (hexa > 0)
+		dest /= 2;
 	db.dsize = 0;
-	db.dptr = (char *) malloc(strlen(s) * sizeof(char));
+	db.dptr = malloc(dest);
 	if (!db.dptr)
 	    oops("cannot get memory");
 
-	for (p = db.dptr; *s != '\0'; p++, db.dsize++, s++) {
-		if (*s == '\\') {
-			if (*++s == 'n')
-				*p = '\n';
-			else if (*s == 'r')
-				*p = '\r';
-			else if (*s == 'f')
-				*p = '\f';
-			else if (*s == 't')
-				*p = '\t';
-			else if (isdigit((unsigned char)*s)
-				 && isdigit((unsigned char)*(s + 1))
-				 && isdigit((unsigned char)*(s + 2)))
-			{
-				i = (*s++ - '0') << 6;
-				i |= (*s++ - '0') << 3;
-				i |= *s - '0';
-				*p = i;
+	if (hexa > 0) {
+		size_t decoded;
+
+		if (len != (len & ~0x1)) {
+			fprintf(stderr, "Hexadecimal for %s not even\n", what);
+			exit(-1);
+		}
+
+		decoded = base16_decode(db.dptr, dest, s, len);
+		if ((size_t) -1 == decoded) {
+			fprintf(stderr, "Invalid hexadecimal %s\n", what);
+			exit(-1);
+		}
+		db.dsize = decoded;
+		return db;
+	} else if (hexa < 0) {
+		memcpy(db.dptr, s, dest);
+	} else {
+		char *p;
+		int i;
+
+		for (p = db.dptr; *s != '\0'; p++, db.dsize++, s++) {
+			if (*s == '\\') {
+				if (*++s == 'n')
+					*p = '\n';
+				else if (*s == 'r')
+					*p = '\r';
+				else if (*s == 'f')
+					*p = '\f';
+				else if (*s == 't')
+					*p = '\t';
+				else if (isdigit((unsigned char)*s)
+					 && isdigit((unsigned char)*(s + 1))
+					 && isdigit((unsigned char)*(s + 2)))
+				{
+					i = (*s++ - '0') << 6;
+					i |= (*s++ - '0') << 3;
+					i |= *s - '0';
+					*p = i;
+				}
+				else if (*s == '0')
+					*p = '\0';
+				else
+					*p = *s;
 			}
-			else if (*s == '0')
-				*p = '\0';
 			else
 				*p = *s;
 		}
-		else
-			*p = *s;
 	}
 
 	return db;
 }
 
+
+datum
+fread_datum(FILE *f, int hexa, const char *what)
+{
+	filestat_t buf;
+	datum db;
+	size_t len, dest;
+
+	if (-1 == fstat(fileno(f), &buf)) {
+		fprintf(stderr, "Cannot get %s length: %s\n", what, g_strerror(errno));
+		exit(-1);
+	}
+
+	dest = len = buf.st_size;
+	if (hexa)
+		dest /= 2;
+	db.dsize = 0;
+	db.dptr = malloc(dest);
+	if (!db.dptr)
+	    oops("cannot get memory");
+
+	if (hexa <= 0) {
+		db.dsize = fread(db.dptr, 1, len, f);
+	} else {
+		void *data = malloc(len);
+		size_t decoded;
+		if (!data)
+			oops("cannot get memory");
+
+		if (len != (len & ~0x1)) {
+			fprintf(stderr, "Hexadecimal for %s not even\n", what);
+			exit(-1);
+		}
+
+		len = fread(data, 1, len, f);
+		decoded = base16_decode(db.dptr, dest, data, len & ~0x1);
+		if ((size_t) -1 == decoded) {
+			fprintf(stderr, "Invalid hexadecimal %s\n", what);
+			exit(-1);
+		}
+		db.dsize = decoded;
+		free(data);
+	}
+
+	return db;
+}
 
 char *
 key2s(datum db)
@@ -200,29 +284,45 @@ key2s(datum db)
 	return buf;
 }
 
+static void
+log_keyerr(datum key, int key_hexa, const char *what)
+{
+	int saved = errno;
+
+	fprintf(stderr, "Error when %s ", what);
+	print_datum(stderr, key, key_hexa);
+	fprintf(stderr, ": %s\n", g_strerror(saved));
+}
+
 char *progname;
 
 static void G_GNUC_NORETURN
 usage(void)
 {
 	fprintf(stderr,
-		"Usage: %s database "
-		"[-m r|w|rw] [-crtX] -a|-d|-f|-F|-s [key [content]]\n", progname);
+		"Usage: %s -a|-c|-d|-f|-F|-s database "
+		"[-m r|w|rw] [-bikortxXy] [key [content]]\n", progname);
 	fprintf(stderr,
-		"  -a : list all entries in the database.\n"
+		"  -a : list all entries (as \"key: value\") in the database.\n"
+		"  -b : content given / output as binary.\n"
 		"  -c : create the database if it does not exist.\n"
 		"  -d : delete the entry associated with key.\n"
 		"  -f : fetch and display the entry associated with key.\n"
 		"  -F : fetch and display all the entries whose key "
 				"matches regular expression.\n"
+		"  -i : input comes from content, interpreted as a filename.\n"
+		"  -k : list all keys in the database.\n"
 		"  -m : specifies database opening mode: "
 				"read-only, write-only, read-write.\n");
 	fprintf(stderr,
+		"  -o : output sent to content, interpreted as a filename.\n"
 		"  -r : replace the entry at key if it already exists (see -s).\n"
 		"  -s : store entry under key provided it does not already exist.\n"
 		"  -t : re-initialize the database before executing the command.\n"
 		"  -v : verbose mode: logs stores and deletions.\n"
-		"  -X : create database with O_EXCL to abort if it already exists.\n");
+		"  -x : keys interpreted / shown as hexadecimal.\n"
+		"  -X : create database with O_EXCL to abort if it already exists.\n"
+		"  -y : content interpreted / shown as hexadecimal.\n");
 	exit(-1);
 }
 
@@ -236,6 +336,10 @@ main(int argc, char **argv)
 	int flags;
 	int giveusage = 0;
 	int verbose = 0;
+	int key_hexa = 0;
+	int content_hexa = 0;
+	int content_is_file = 0;
+	int key_only = 0;
 	commands what = HELP;
 	char *comarg[3];
 	int st_flag = DBM_INSERT;
@@ -244,15 +348,23 @@ main(int argc, char **argv)
 	datum key;
 	datum content;
 	regex_t re;
+	char *mode = NULL;
+	FILE *f = stdout;
 
 	progname = argv[0];
 	flags = O_RDWR;
 	argn = 0;
 
-	while ((opt = my_getopt(argc, argv, "acdfFm:rstvX")) != ':') {
+	while ((opt = my_getopt(argc, argv, "abcdfFikm:orstvxXy")) != ':') {
 		switch (opt) {
+		case 'k':
+			key_only = 1;
+			break;
 		case 'a':
 			what = SCAN;
+			break;
+		case 'b':
+			content_hexa = -1;
 			break;
 		case 'c':
 			flags |= O_CREAT;
@@ -265,6 +377,10 @@ main(int argc, char **argv)
 			break;
 		case 'F':
 			what = REGEXP;
+			break;
+		case 'i':
+			mode = "rb";
+			content_is_file = 1;
 			break;
 		case 'm':
 			flags &= ~(000007);
@@ -279,9 +395,13 @@ main(int argc, char **argv)
 				giveusage = 1;
 			}
 			break;
+		case 'o':
+			mode = "wb";
+			content_is_file = 1;
+			break;
 		case 'r':
 			st_flag = DBM_REPLACE;
-			break;
+			/* FALL THROUGH */
 		case 's':
 			what = STORE;
 			break;
@@ -291,8 +411,14 @@ main(int argc, char **argv)
 		case 'v':
 			verbose = 1;
 			break;
+		case 'x':
+			key_hexa = 1;
+			break;
 		case 'X':
 			flags |= O_EXCL;
+			break;
+		case 'y':
+			content_hexa = 1;
 			break;
 		case '!':
 			giveusage = 1;
@@ -308,19 +434,42 @@ main(int argc, char **argv)
 		}
 	}
 
-	if (giveusage || what == HELP || argn < 1) {
+	if (key_only && HELP == what)
+		what = SCAN;
+
+	if (giveusage || what == HELP || argn < 1)
 		usage();
-	}
 
 	if ((db = sdbm_open(comarg[0], flags, 0777)) == NULL) {
-		fprintf(stderr, "Error opening database \"%s\"\n", comarg[0]);
+		fprintf(stderr, "Error opening database \"%s\": %s\n", comarg[0],
+			g_strerror(errno));
 		exit(-1);
 	}
 
 	if (argn > 1)
-		key = read_datum(comarg[1]);
-	if (argn > 2)
-		content = read_datum(comarg[2]);
+		key = read_datum(comarg[1], key_hexa, "key");
+
+	if (argn > 2) {
+		if (content_is_file) {
+			f = fopen(comarg[2], mode);
+			if (NULL == f) {
+				fprintf(stderr, "Cannot open \"%s\" for %s: %s\n",
+					comarg[2], 'r' == *mode ? "reading" : "writing",
+					g_strerror(errno));
+				exit(-1);
+			}
+			if ('r' == *mode) {
+				content = fread_datum(f, content_hexa, "content");
+				fclose(f);
+				f = stdout;
+			}
+		} else {
+			content = read_datum(comarg[2], content_hexa, "content");
+		}
+	} else if (content_is_file) {
+		fprintf(stderr, "WARNING: ignoring spurious -%c option\n",
+			'r' == *mode ? 'i' : 'o');
+	}
 
 	switch (what) {
 	case HELP:
@@ -328,23 +477,25 @@ main(int argc, char **argv)
 	case SCAN:
 		key = sdbm_firstkey(db);
 		if (sdbm_error(db)) {
-			fprintf(stderr, "Error when fetching first key\n");
+			fprintf(stderr, "Error when fetching first key: %s\n",
+				g_strerror(errno));
 			goto db_exit;
 		}
 		while (key.dptr != NULL) {
 			content = sdbm_fetch(db, key);
 			if (sdbm_error(db)) {
-				fprintf(stderr, "Error when fetching ");
-				print_datum(key);
-				printf("\n");
+				log_keyerr(key, key_hexa, "fetching");
 				goto db_exit;
 			}
-			print_datum(key);
-			printf(": ");
-			print_datum(content);
-			printf("\n");
+			print_datum(f, key, key_hexa);
+			if (!key_only) {
+				fprintf(f, ": ");
+				print_datum(f, content, content_hexa);
+			}
+			fprintf(f, "\n");
 			if (sdbm_error(db)) {
-				fprintf(stderr, "Error when fetching next key\n");
+				fprintf(stderr, "Error when fetching next key: %s\n",
+					g_strerror(errno));
 				goto db_exit;
 			}
 			key = sdbm_nextkey(db);
@@ -362,23 +513,26 @@ main(int argc, char **argv)
 		}
 		key = sdbm_firstkey(db);
 		if (sdbm_error(db)) {
-			fprintf(stderr, "Error when fetching first key\n");
+			fprintf(stderr, "Error when fetching first key: %s\n",
+				g_strerror(errno));
 			goto db_exit;
 		}
 		while (key.dptr != NULL) {
 			char *str = key2s(key);
-			if (regexec(&re, str, 0, NULL, 0)) {
+			if (0 == regexec(&re, str, 0, NULL, 0)) {
 				content = sdbm_fetch(db, key);
 				if (sdbm_error(db)) {
 					fprintf(stderr, "Error when fetching ");
-					print_datum(key);
-					printf("\n");
+					print_datum(stderr, key, key_hexa);
+					fprintf(stderr, ": %s\n", g_strerror(errno));
 					goto db_exit;
 				}
-				print_datum(key);
-				printf(": ");
-				print_datum(content);
-				printf("\n");
+				print_datum(f, key, key_hexa);
+				if (!key_only) {
+					fprintf(f, ": ");
+					print_datum(f, content, content_hexa);
+				}
+				fprintf(f, "\n");
 				if (sdbm_error(db)) {
 					fprintf(stderr, "Error when fetching next key\n");
 					goto db_exit;
@@ -396,21 +550,23 @@ main(int argc, char **argv)
 		}
 		content = sdbm_fetch(db, key);
 		if (sdbm_error(db)) {
-			fprintf(stderr, "Error when fetching ");
-			print_datum(key);
-			printf("\n");
+			log_keyerr(key, key_hexa, "fetching");
 			goto db_exit;
 		}
 		if (content.dptr == NULL) {
 			fprintf(stderr, "Cannot find ");
-			print_datum(key);
-			printf("\n");
+			print_datum(stderr, key, key_hexa);
+			fprintf(stderr, "\n");
 			goto db_exit;
 		}
-		print_datum(key);
-		printf(": ");
-		print_datum(content);
-		printf("\n");
+		if (content_is_file) {
+			print_datum(f, content, content_hexa);
+		} else {
+			print_datum(f, key, key_hexa);
+			fprintf(f, ": ");
+			print_datum(f, content, content_hexa);
+			fprintf(f, "\n");
+		}
 		break;
 
 	case DELETE:
@@ -419,14 +575,12 @@ main(int argc, char **argv)
 			goto db_exit;
 		}
 		if (sdbm_delete(db, key) || sdbm_error(db)) {
-			fprintf(stderr, "Error when deleting ");
-			print_datum(key);
-			printf("\n");
+			log_keyerr(key, key_hexa, "deleting");
 			goto db_exit;
 		}
 		if (verbose) {
-			print_datum(key);
-			printf(": DELETED\n");
+			print_datum(f, key, key_hexa);
+			fprintf(f, ": DELETED\n");
 		}
 		break;
 
@@ -436,25 +590,27 @@ main(int argc, char **argv)
 			goto db_exit;
 		}
 		if (sdbm_store(db, key, content, st_flag) || sdbm_error(db)) {
-			fprintf(stderr, "Error when storing ");
-			print_datum(key);
-			printf("\n");
+			log_keyerr(key, key_hexa, "storing");
 			goto db_exit;
 		}
 		if (verbose) {
-			print_datum(key);
-			printf(": ");
-			print_datum(content);
-			printf(" STORED\n");
+			print_datum(f, key, key_hexa);
+			fprintf(f,": ");
+			print_datum(f, content, content_hexa);
+			fprintf(f, " STORED\n");
 		}
 		break;
 	}
 
 db_exit:
+	if (content_is_file)
+		fclose(f);
+
 	sdbm_clearerr(db);
 	sdbm_close(db);
 	if (sdbm_error(db)) {
-		fprintf(stderr, "Error closing database \"%s\"\n", comarg[0]);
+		fprintf(stderr, "Error closing database \"%s\": %s\n", comarg[0],
+			g_strerror(errno));
 		exit(-1);
 	}
 
