f7efd5625b29fabe02aff68ac71a244d9aecfb45
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2012-01-14 22:08:34 +0100

    Replaced KUID arithmetic with new big integer arithmetic.

--- src/dht/kuid.h
+++ src/dht/kuid.h
@@ -52,20 +52,7 @@ gboolean kuid_match_nth(const kuid_t *k1, const kuid_t *k2, int bits)
 size_t kuid_common_prefix(const kuid_t *k1, const kuid_t *k2) G_GNUC_PURE;
 void kuid_random_within(kuid_t *dest, const kuid_t *prefix, int bits);
 void kuid_flip_nth_leading_bit(kuid_t *res, int n);
-
 void kuid_zero(kuid_t *res);
-void kuid_not(kuid_t *k);
-void kuid_set32(kuid_t *res, guint32 val);
-void kuid_set64(kuid_t *res, guint64 val);
-void kuid_set_nth_bit(kuid_t *res, int n);
-gboolean kuid_add(kuid_t *res, const kuid_t *other);
-gboolean kuid_add_u8(kuid_t *k, guint8 l);
-gboolean kuid_lshift(kuid_t *res);
-void kuid_rshift(kuid_t *res);
-guint8 kuid_mult_u8(kuid_t *res, guint8 l);
-void kuid_divide(const kuid_t *k1, const kuid_t *k2, kuid_t *q, kuid_t *r);
-double kuid_to_double(const kuid_t *value);
-guint64 kuid_to_guint64(const kuid_t *value);
 
 /**
  * Return leading KUID byte.
