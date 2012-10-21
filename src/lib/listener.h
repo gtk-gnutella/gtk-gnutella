b59c6eb60c6d33e7765186e742c735d1bba22ff1
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2012-10-21 13:29:54 +0200

    Added extra documentation (full working example) for future usage reference.

--- src/lib/listener.h
+++ src/lib/listener.h
@@ -37,6 +37,8 @@
 #include "common.h"
 
 /**
+ * OVERVIEW
+ *
  * To use the macros below with a "node_added" signal for example,
  * you need to have a storage stucture to hold the listeners list
  * This needs to be defined in the following fashion. The name is
@@ -50,7 +52,91 @@
  *
  * typedef void (*node_added_listener_t) (gnutella_node_t *, const char *);
  *
- * Again the name is important (like above).
+ * Again the name prefix is important (like above).
+ *
+ * EXAMPLE
+ *
+ * Here's a full working example showing how to use listening interface.
+ *
+ * We want to monitor the creation of GUESS queries from the outside, without
+ * the GUESS code knowing who is monitoring it exactly.
+ *
+ * - We declare the following in the GUESS layer (guess.h):
+ *
+ * enum guess_mode {
+ *     GUESS_QUERY_BOUNDED,
+ *     GUESS_QUERY_LOOSE
+ * };
+ *
+ * struct guess_query {
+ *     size_t max_ultra;
+ *     enum guess_mode mode;
+ * };
+ *
+ * typedef void (*guess_event_listener_t)(gnet_search_t,
+ *     const struct guess_query *query);
+ *
+ * void guess_event_listener_add(guess_event_listener_t);
+ * void guess_event_listener_remove(guess_event_listener_t);
+ *
+ * - We implement the following in the GUESS layer (guess.c):
+ *
+ * static listeners_t guess_event_listeners;
+ *
+ * void guess_event_listener_add(guess_event_listener_t l) {
+ *     LISTENER_ADD(guess_event, l);
+ * }
+ *
+ * void guess_event_listener_remove(guess_event_listener_t l) {
+ *     LISTENER_REMOVE(guess_event, l);
+ * }
+ *
+ * static void
+ * guess_event_fire(const guess_t *gq, bool created)
+ * {
+ *     struct guess_query query;
+ *
+ *     query.max_ultra = gq->max_ultrapeers;
+ *     query.mode      = gq->mode;
+ *
+ *     LISTENER_EMIT(guess_event, (gq->sh, created ? &query : NULL));
+ * }
+ *
+ * - We use it as follows from the GUESS layer:
+ *
+ * guess_event_fire(gq, TRUE);	// Each time a new query is created
+ *
+ * - We register listeners externally like this, from the GUI for instance:
+ *
+ * guess_event_listener_add(gui_guess_event);
+ *
+ * - We implement the listening callback in the GUI:
+ *
+ * static void
+ * gui_guess_event(gnet_search_t sh, const struct guess_query *query)
+ * {
+ *		// whatever needs to be done
+ * }
+ *
+ * NOTES
+ *
+ * The parameters for the callback need to be enclosed in parentheses within
+ * the second LISTENER_EMIT() macro argument, according to the defined
+ * signature for that particular callback -- each event can have a different
+ * signature!
+ *
+ * There is a fair amount of plumbering required, but this pattern allows
+ * total separation of concerns between the GUESS and the GUI modules: the
+ * GUESS layer does not know who is listening to events (there could be
+ * multiple parties interested) and the GUI only needs to register its
+ * listener but does not need to know when events are fired.  There is no
+ * access of the GUESS internals by the callback: all the necessary data for
+ * the listener is copied by the GUESS layer. 
+ *
+ * The only dependency is in the listener registering/removal interface that
+ * the GUESS layer has to provide, plus the definition of the parameters that
+ * will be passed to the callback.  And of course, the semantics of the
+ * events triggered must be clearly known.
  */
 
 typedef GSList *listeners_t;
