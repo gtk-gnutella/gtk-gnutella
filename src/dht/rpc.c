gtk-gnutella/gtk-gnutella

commit 9d57764d26e5edb7037697e1581f1e908bf9d8fb

DHT: on RPC reply KUID mismatches, remove the OLD node from the routing table.
We were wrongly removing the replying node, which bears the new KUID and left
the old one in the routing table.

Also use this opportunity to update the "stable node" cache with the new KUID
for the replying node in case we need to store anything there, making sure to
remove the old entry bearing the now obsolete KUID.

src/dht/rpc.c
@@ -34,9 +34,10 @@
 #include "common.h"
 
 #include "rpc.h"
-#include "routing.h"
 #include "kmsg.h"
 #include "knode.h"
+#include "routing.h"
+#include "stable.h"
 
 #include "if/gnet_property_priv.h"
 
@@ -390,10 +391,12 @@ dht_rpc_answer(const guid_t *muid,
 		/*
 		 * This node is stale: the node to which we sent the RPC bears
 		 * a KUID different from the one we thought it had.  The node we
-		 * knew about is therefore gone.
+		 * knew about is therefore gone and has a new KUID.
 		 *
 		 * Remove the original node from the routing table (in case it is
-		 * present) and do not handle the reply.
+		 * present) and do not handle the reply: that would be misleading
+		 * if we are performing a node lookup for instance, because we do not
+		 * want to enter the new node in the path.
 		 */
 
 		gnet_stats_inc_general(GNR_DHT_RPC_KUID_REPLY_MISMATCH);
@@ -406,8 +409,9 @@ dht_rpc_answer(const guid_t *muid,
 				knode_to_string2(kn));
 		}
 
-		dht_remove_node(rn);				/* Discard obsolete entry */
-		rpc_timed_out(cq_main(), rcb);		/* Invoke user callback if any */
+		stable_replace(kn, rn);			/* KUID was changed */
+		dht_remove_node(kn);			/* Remove obsolete entry from routing */
+		rpc_timed_out(cq_main(), rcb);	/* Invoke user callback if any */
 
 		return FALSE;	/* RPC was sent to wrong node, ignore */
 	}
@@ -461,7 +465,7 @@ dht_rpc_answer(const guid_t *muid,
 	 * found at RPC reply time, a new node in "unknown" status was created.
 	 */
 
-	if (kn->status == KNODE_STALE) {
+	if (KNODE_STALE == kn->status) {
 		dht_set_node_status(kn, KNODE_GOOD);
 		gnet_stats_inc_general(GNR_DHT_REVITALIZED_STALE_NODES);
 	}

src/dht/stable.c
@@ -310,6 +310,40 @@ stable_record_activity(const knode_t *kn)
 }
 
 /**
+ * The KUID of the node has changed: remove its entry if it had one and make
+ * sure we have an entry for the new KUID.
+ *
+ * @param kn		the old node
+ * @param rn		the replacing node
+ */
+void
+stable_replace(const knode_t *kn, const knode_t *rn)
+{
+	struct lifedata *ld;
+
+	knode_check(kn);
+	knode_check(rn);
+	g_assert(rn->flags & KNODE_F_ALIVE);
+
+	ld = get_lifedata(kn->id);
+	if (NULL == ld)
+		return;				/* Node was not recorded in the "stable" set */
+
+	if (GNET_PROPERTY(dht_stable_debug)) {
+		g_debug("DHT STABLE removing obsolete %s, now at %s",
+			knode_to_string(kn), knode_to_string2(rn));
+	}
+
+	/*
+	 * Remove the old node and create an entry for the new one.
+	 */
+
+	dbmw_delete(db_lifedata, kn->id->v);
+	gnet_stats_dec_general(GNR_DHT_STABLE_NODES_HELD);
+	stable_record_activity(rn);
+}
+
+/**
  * Estimate probability of presence for a value published to some roots in
  * a given time frame.
  *

src/dht/stable.h
@@ -49,6 +49,7 @@ double stable_alive_probability(time_delta_t t, time_delta_t d);
 double stable_still_alive_probability(time_t first_seen, time_t last_seen);
 
 void stable_record_activity(const knode_t *kn);
+void stable_replace(const knode_t *kn, const knode_t *rn);
 
 #endif /* _dht_stable_h_ */
 

commit 9192b6069533df2efb3e2614bcbe0436be2acffe

dht_rpc_answer(): mistakenly switched roles of kn and rn.
This error was introduced in 9d57764d26e5edb7037697e1581f1e908bf9d8fb.

Also avoid assertion failure on KUID mismatch on RPC replies because nodes
are supposed to be flagged as "ALIVE" when they are stored in the stable
database.

src/dht/rpc.c
@@ -409,8 +409,12 @@ dht_rpc_answer(const guid_t *muid,
 				knode_to_string2(kn));
 		}
 
-		stable_replace(kn, rn);			/* KUID was changed */
-		dht_remove_node(kn);			/* Remove obsolete entry from routing */
+		/* Mark alive for stable_replace() */
+		rn->flags |= KNODE_F_ALIVE;		/* Receiving RPC reply traffic */
+		kn->flags |= KNODE_F_ALIVE;		/* Got traffic from that node */
+
+		stable_replace(rn, kn);			/* KUID of rn was changed */
+		dht_remove_node(rn);			/* Remove obsolete entry from routing */
 		rpc_timed_out(cq_main(), rcb);	/* Invoke user callback if any */
 
 		return FALSE;	/* RPC was sent to wrong node, ignore */