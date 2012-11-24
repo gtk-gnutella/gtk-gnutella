9f0fecfc23d6bc969aa3c3fc1e6744678894ff2f
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2012-11-24 18:42:30 +0100

    assertion_warning_log(): was incorrectly logging assertion with a FATAL tag.
    A soft assertion failure must be logged with a WARNING tag.

--- src/lib/fast_assert.c
+++ src/lib/fast_assert.c
@@ -183,7 +183,7 @@ assertion_warning_log(const assertion_data * const data,
 	static str_t *str;
 	va_list args;
 
-	assertion_message(data, TRUE);
+	assertion_message(data, FALSE);
 
 	if G_UNLIKELY(NULL == str)
 		str = str_new_not_leaking(512);
