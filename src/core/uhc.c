4bfb90920bc761998183f62c68c7418cff7a0835
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2013-05-07 22:19:57 +0200

    Updated bootstrapping UHC list.

--- src/core/uhc.c
+++ src/core/uhc.c
@@ -99,7 +99,7 @@ static const struct {
 #if defined(USE_LOCAL_UHC)
 	{ "localhost:6346" },
 #else	/* !USE_LOCAL_UHC */
-	{ "uhc.shacknet.nu:19104" },
+	{ "1.uhc.gtk-gnutella.nl:19104" },
 	{ "uhc.gtk-gnutella.nl:15749" },
 #endif	/* USE_LOCAL_UHC */
 };
