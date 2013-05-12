65b10b3bce0f31a94d49b92482c1e0170a2154fa
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2013-05-12 10:49:23 +0200

    Added specifications for strlcpy() and strlcat() from the BSD man page.

--- src/lib/glib-missing.h
+++ src/lib/glib-missing.h
@@ -105,6 +105,53 @@ void g_main_context_set_poll_func(GMainContext *context, GPollFunc func);
  */
 #ifndef USE_GLIB2
 
+/*
+ * (Copied from BSD man pages)
+ *
+ * NAME
+ *
+ *   strlcpy, strlcat -- size-bounded string copying and concatenation
+ *
+ * DESCRIPTION
+ *
+ *   The strlcpy() and strlcat() functions copy and concatenate strings
+ *   respectively.  They are designed to be safer, more consistent, and less
+ *   error prone replacements for strncpy(3) and strncat(3).  Unlike those
+ *   functions, strlcpy() and strlcat() take the full size of the buffer (not
+ *   just the length) and guarantee to NUL-terminate the result (as long as
+ *   size is larger than 0 or, in the case of strlcat(), as long as there is
+ *   at least one byte free in dst).  Note that a byte for the NUL should be
+ *   included in size.  Also note that strlcpy() and strlcat() only operate on
+ *   true ``C'' strings.  This means that for strlcpy() src must be NUL-termi-
+ *   nated and for strlcat() both src and dst must be NUL-terminated.
+ * 
+ *   The strlcpy() function copies up to size - 1 characters from the NUL-ter-
+ *   minated string src to dst, NUL-terminating the result.
+ * 
+ *   The strlcat() function appends the NUL-terminated string src to the end
+ *   of dst.  It will append at most size - strlen(dst) - 1 bytes, NUL-termi-
+ *   nating the result.
+ * 
+ *   The source and destination strings should not overlap, as the behavior is
+ *   undefined.
+ *
+ * RETURN VALUES
+ *
+ *   The strlcpy() and strlcat() functions return the total length of the
+ *   string they tried to create.  For strlcpy() that means the length of src.
+ *   For strlcat() that means the initial length of dst plus the length of
+ *   src.  While this may seem somewhat confusing, it was done to make trunca-
+ *   tion detection simple.
+ *
+ *   Note however, that if strlcat() traverses size characters without finding
+ *   a NUL, the length of the string is considered to be size and the destina-
+ *   tion string will not be NUL-terminated (since there was no space for the
+ *   NUL).  This keeps strlcat() from running off the end of a string.  In
+ *   practice this should not happen (as it means that either size is incor-
+ *   rect or that dst is not a proper ``C'' string).  The check exists to pre-
+ *   vent potential security problems in incorrect code.
+ */
+
 #ifndef HAS_STRLCPY
 size_t strlcpy(char *dst, const char *src, size_t dst_size);
 #endif /* HAS_STRLCPY */
