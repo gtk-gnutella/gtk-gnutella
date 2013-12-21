7608106eca7d17b1f3c09a5bb51ab5e4981e0e31
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2013-12-21 15:36:06 +0100

    once_flag_run(): can now release the lock during routine execution.
    Holding the (global) lock during the routine execution could lead to deadlocks
    if two distinct threads need to perform two different initializations, yet
    the first thread grabs a lock during its initialization, lock that a second
    thread already holds when it attempts to call once_flag_run().

--- src/lib/once.c
+++ src/lib/once.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012 Raphael Manfredi
+ * Copyright (c) 2012-2013 Raphael Manfredi
  *
  *----------------------------------------------------------------------
  * This file is part of gtk-gnutella.
@@ -28,7 +28,7 @@
  * Thread-safe once initialization support.
  *
  * @author Raphael Manfredi
- * @date 2012
+ * @date 2012-2013
  */
 
 #include "common.h"
@@ -36,10 +36,37 @@
 #include "once.h"
 
 #include "atomic.h"
+#include "compat_usleep.h"
 #include "cond.h"
+#include "hashtable.h"
 #include "mutex.h"
+#include "thread.h"
 
 #include "override.h"			/* Must be the last header included */
+
+#define ONCE_DELAY		200			/* Wait 200 us before looping again */
+#define ONCE_TIMEOUT	10000000	/* 10 seconds, in us */
+#define ONCE_LOOP_MAX	(ONCE_TIMEOUT / ONCE_DELAY)
+
+/**
+ * Arena to be used for the hash table keeping track of the pending
+ * initializations.  It is large enough to let the table hold 32 entries
+ * on a 64-bit machine, which is plenty enough.
+ */
+static char once_buffer[1024];
+
+/**
+ * Hash table tracking the pending initializations.
+ *
+ * Keys are the init routine to be run, the value is the ID of the thread.
+ */
+static hash_table_t *once_running;
+
+/**
+ * Global mutex protecting access to the user-supplied variable and to the
+ * once_running table.
+ */
+static mutex_t once_flag_mtx = MUTEX_INIT;
 
 /**
  * Execute supplied routine once, as tracked by the supplied flag.
@@ -49,8 +76,8 @@
  * in a thread-safe way, whether the routine has been run.
  *
  * @attention
- * A global mutex is held during the execution of the routine.  If the
- * calling thread can block, use once_flag_runwait() instead.
+ * If the calling thread can block, and memory can be allocated, use the
+ * once_flag_runwait() routine instead, as it will avoid "busy" waits.
  *
  * @param flag		control flag, initially set to FALSE
  * @param routine	the routine to run if it has not been done already
@@ -60,8 +87,7 @@
 bool
 once_flag_run(once_flag_t *flag, once_fn_t routine)
 {
-	static mutex_t once_flag_mtx = MUTEX_INIT;
-	static int recursion;
+	int id;
 
 	if G_LIKELY(ONCE_F_DONE == *flag)
 		return FALSE;
@@ -73,27 +99,53 @@ once_flag_run(once_flag_t *flag, once_fn_t routine)
 		return FALSE;
 	}
 
+	if G_UNLIKELY(NULL == once_running)
+		once_running = hash_table_new_fixed(once_buffer, sizeof once_buffer);
+
+	id = thread_small_id();
+
 	if G_UNLIKELY(ONCE_F_PROGRESS == *flag) {
-		if (recursion++ > 2)
-			s_minierror("%s(): endless recursive attempt to initialize "
-				"routine %p", G_STRFUNC, routine);
+		int stid = pointer_to_int(hash_table_lookup(once_running, routine));
+		size_t n;
 
-		if (1 == recursion)
-			s_miniwarn("%s(): recursive attempt to initialize %s()",
+		/*
+		 * If we detect a recursive initialization, terminate the process.
+		 * Otherwise, we have to wait until the flag becomes ONCE_F_DONE.
+		 */
+
+		if (stid == id) {
+			s_minierror("%s(): recursive attempt to initialize routine %s()",
 				G_STRFUNC, stacktrace_function_name(routine));
+		}
 
-		/* Cheat, mark it done since we're recursing */
+		for (n = 0; n < ONCE_LOOP_MAX && ONCE_F_PROGRESS == *flag; n++) {
+			mutex_unlock(&once_flag_mtx);
+			compat_usleep_nocancel(200);
+			mutex_lock(&once_flag_mtx);
+		}
 
-		*flag = ONCE_F_DONE;
+		if (ONCE_F_PROGRESS == *flag) {
+			s_warning("%s(): timeout waiting for completion of %s() by %s",
+				G_STRFUNC, stacktrace_function_name(routine),
+				thread_id_name(stid));
+			thread_lock_dump_all(STDERR_FILENO);
+			s_minierror("%s(): %s() timed out", G_STRFUNC, thread_name());
+		}
+
+		g_assert(ONCE_F_DONE == *flag);
 		mutex_unlock(&once_flag_mtx);
 		return FALSE;
 	}
 
 	*flag = ONCE_F_PROGRESS;
-	atomic_mb();
+	hash_table_insert(once_running, routine, int_to_pointer(id));
+	mutex_unlock(&once_flag_mtx);
+
 	(*routine)();
+
+	mutex_lock(&once_flag_mtx);
 	*flag = ONCE_F_DONE;
-	recursion = 0;
+	hash_table_remove(once_running, routine);
 	mutex_unlock(&once_flag_mtx);
 
 	return TRUE;
@@ -107,8 +159,10 @@ once_flag_run(once_flag_t *flag, once_fn_t routine)
  * in a thread-safe way, whether the routine has been run.
  *
  * @attention
- * The calling thread can block but no mutex is held during the processing
- * of the initialization routine.
+ * The calling thread can block but it must not hold any lock prior to
+ * calling this routine.  If this cannot be ensured, then use once_flag_run()
+ * which will perform a "busy" wait but does not prevent the calling thread
+ * from already holding a lock.
  *
  * @param flag		control flag, initially set to FALSE
  * @param routine	the routine to run if it has not been done already
@@ -118,8 +172,9 @@ once_flag_run(once_flag_t *flag, once_fn_t routine)
 bool
 once_flag_runwait(once_flag_t *flag, once_fn_t routine)
 {
-	static mutex_t once_flag_mtx = MUTEX_INIT;
 	static cond_t once_cond = COND_INIT;
+	int id;
+	bool is_inserted;
 
 	if G_LIKELY(ONCE_F_DONE == *flag)
 		return FALSE;
@@ -131,12 +186,29 @@ once_flag_runwait(once_flag_t *flag, once_fn_t routine)
 		return FALSE;
 	}
 
+	if G_UNLIKELY(NULL == once_running)
+		once_running = hash_table_new_fixed(once_buffer, sizeof once_buffer);
+
+	id = thread_small_id();
+
 	/*
 	 * If another thread is in the process of running the initialization
 	 * routine, wait until it is completed, at which point we can return.
 	 */
 
 	if G_UNLIKELY(ONCE_F_PROGRESS == *flag) {
+		int stid = pointer_to_int(hash_table_lookup(once_running, routine));
+
+		/*
+		 * If we detect a recursive initialization, terminate the process.
+		 * Otherwise, we have to wait until the flag becomes ONCE_F_DONE.
+		 */
+
+		if (stid == id) {
+			s_minierror("%s(): recursive attempt to initialize routine %s()",
+				G_STRFUNC, stacktrace_function_name(routine));
+		}
+
 		while (ONCE_F_PROGRESS == *flag)
 			cond_wait_clean(&once_cond, &once_flag_mtx);
 
@@ -151,7 +223,10 @@ once_flag_runwait(once_flag_t *flag, once_fn_t routine)
 	 */
 
 	*flag = ONCE_F_PROGRESS;
+	is_inserted = hash_table_insert(once_running, routine, int_to_pointer(id));
 	mutex_unlock(&once_flag_mtx);
+
+	g_assert(is_inserted);	/* Key was not already present in the hash table */
 
 	(*routine)();
 
@@ -169,6 +244,7 @@ once_flag_runwait(once_flag_t *flag, once_fn_t routine)
 
 	mutex_lock(&once_flag_mtx);
 	*flag = ONCE_F_DONE;
+	hash_table_remove(once_running, routine);
 	cond_broadcast(&once_cond, &once_flag_mtx);
 	cond_reset(&once_cond);
 	mutex_unlock(&once_flag_mtx);
