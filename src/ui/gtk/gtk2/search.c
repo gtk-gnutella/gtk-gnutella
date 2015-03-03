a59d7210eb4d62fea9d551f58b61ff494646fb49
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2015-03-03 14:36:46 +0100

    search_gui_update_list_label(): show inactive search immedialtely.
    Regardless of the presence of unseen hits, display the inactive background
    color when the search becomes inactive.

--- src/ui/gtk/gtk2/search.c
+++ src/ui/gtk/gtk2/search.c
@@ -1334,13 +1334,15 @@ search_gui_update_list_label(const struct search *search)
 	style = gtk_widget_get_style(GTK_WIDGET(tv));
 	downloading = 0 != search_gui_download_count(search);
 
-	if (search->unseen_items > 0) {
-		fg = downloading ? gui_color_get(GUI_COLOR_DOWNLOADING) :
-				&style->fg[GTK_STATE_ACTIVE];
-		bg = &style->bg[GTK_STATE_ACTIVE];
-	} else if (search_gui_is_enabled(search)) {
-		fg = downloading ? gui_color_get(GUI_COLOR_DOWNLOADING) : NULL;
-		bg = NULL;
+	if (search_gui_is_enabled(search)) {
+		if (search->unseen_items > 0) {
+			fg = downloading ? gui_color_get(GUI_COLOR_DOWNLOADING) :
+					&style->fg[GTK_STATE_ACTIVE];
+			bg = &style->bg[GTK_STATE_ACTIVE];
+		} else {
+			fg = downloading ? gui_color_get(GUI_COLOR_DOWNLOADING) : NULL;
+			bg = NULL;
+		}
 	} else {
 		fg = downloading ? gui_color_get(GUI_COLOR_DOWNLOADING) :
 				&style->fg[GTK_STATE_INSENSITIVE];
