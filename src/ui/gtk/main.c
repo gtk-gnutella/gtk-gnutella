f2c0273fa50317b603db24b92416c7a6e526456e
Jeroen Asselman <jeroen@asselman.com>
2015-03-14 20:19:26 +0100

    Support newer gtkmacintegration

--- src/ui/gtk/main.c
+++ src/ui/gtk/main.c
@@ -39,7 +39,7 @@
 #endif /* I_PWD */
 
 #ifdef HAVE_GTKOSXAPPLICATION
-#include <gtkmacintegration/gtkosxapplication.h>
+#include <gtkmacintegration-gtk2/gtkosxapplication.h>
 #include "settings_cb.h"
 #endif
 
