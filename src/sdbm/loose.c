1523b7b5e9470defad8d50f6683345c3419653ab
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2015-10-20 12:23:34 +0200

    loose_process(): only release lock on DB before invoking processing callback.
    When not loosely processing with DBM_F_ALLKEYS, we need to keep the database
    locked whilst reading keys and values in case there is a concurrent update
    happening that causes bytes to be moved around on the page: the modification
    timestamp on the wired cached page is not sufficient here -- we need a larger
    critical section to ensure the data we read are consistent.

--- src/sdbm/loose.c
+++ src/sdbm/loose.c
@@ -37,8 +37,8 @@
  * That is the price to pay, and the application must be ready to cope with
  * these unorthodox conditions.  In return, the iterator can run concurrently
  * in a separate thread and does not need to hold the lock on the database for
- * the whole duration.  Processing callbacks are guaranteed to be invoked with
- * the database being unlocked.
+ * the whole duration.  Processing callbacks are invoked with an unlocked
+ * database if we can afford it.
  *
  * Loose iterators require that the LRU feature be turned on in "tune.h" since
  * we rely on the cache wiring feature.
@@ -159,16 +159,6 @@ loose_process(struct loose_vars *v,
 
 		locked = TRUE;
 		v->stats->locked++;
-	} else {
-		/*
-		 * From now on, we're going to relinquish the lock.  We only need to
-		 * take the lock back when the page we're iterating over is modified
-		 * (as identified by its modification stamp) or when we need to read
-		 * a big key or big value.
-		 */
-
-		sdbm_unsynchronize(v->db);
-		locked = FALSE;
 	}
 
 restart:
@@ -181,11 +171,11 @@ restart:
 	 */
 
 	if G_UNLIKELY(restarted++ > LOOSE_RESTART_MAX) {
+		assert_sdbm_locked(v->db);
 		g_assert(!locked);
-		sdbm_synchronize(v->db);
 		cur_mstamp = lru_wired_mstamp(v->db, pag);
 		v->stats->locked++;
-		locked = TRUE;
+		locked = TRUE;		/* Keep DB locked throughout page processing */
 	}
 
 	/*
@@ -199,7 +189,7 @@ restart:
 	for (n = 0; n < cur_cnt; n++) {
 		datum d, *key = NULL, *value = NULL;
 		struct sdbm_pair *p = &pv[n];
-		bool restart = FALSE;
+		bool restart, deleted = FALSE;
 
 		g_assert(n >= 0 && n < cnt);	/* `cnt', the physical size of pv[] */
 
@@ -217,114 +207,32 @@ restart:
 		}
 
 		/*
-		 * Read the key.
+		 * Read the key, taking a private copy of the bits.
+		 *
+		 * This is critical for big keys (and values) because the data
+		 * returned is held in a scratch buffer whose lifetime is up to
+		 * the next request made on the database.
 		 */
 
 #ifdef BIGDATA
-		if (p->kbig) {
-			/*
-			 * Reading a big key require a lock since we're going to also
-			 * access the .dat file and that requires the DB to be locked.
-			 */
-
-			sdbm_synchronize(v->db);
-
-			if G_LIKELY(cur_mstamp == lru_wired_mstamp(v->db, pag)) {
-				d = getnkey(v->db, pag, n + 1);
-				key = sdbm_datum_copy(&d, &v->key);
-				v->stats->big_keys++;
-			} else {
-				restart = TRUE;
-			}
-
-			sdbm_unsynchronize(v->db);
-		} else
+		if (p->kbig)
+			v->stats->big_keys++;
 #endif	/* BIGDATA */
-		{
-			/*
-			 * Reading a normal inlined key from our wired page does not
-			 * require a lock: we cannot read outside of the page anyway,
-			 * but we can read garbage.  Therefore, we check that the stamp
-			 * has not changed after copying the key bits.
-			 */
 
-			d = getnkey(v->db, pag, n + 1);
-			key = sdbm_datum_copy(&d, &v->key);
-			restart = cur_mstamp != lru_wired_mstamp(v->db, pag);
-		}
-
-		if G_UNLIKELY(restart)
-			goto restart_if_needed;		/* Avoid indenting code too much */
+		d = getnkey(v->db, pag, n + 1);
+		key = sdbm_datum_copy(&d, &v->key);
 
 		/*
 		 * Read the value, same logic as code for the key above.
 		 */
 
 #ifdef BIGDATA
-		if (p->vbig) {
-			sdbm_synchronize(v->db);
-
-			if G_LIKELY(cur_mstamp == lru_wired_mstamp(v->db, pag)) {
-				d = getnval(v->db, pag, n + 1);
-				value = sdbm_datum_copy(&d, &v->value);
-				v->stats->big_values++;
-			} else {
-				restart = TRUE;
-			}
-
-			sdbm_unsynchronize(v->db);
-		} else
+		if (p->vbig)
+			v->stats->big_values++;
 #endif	/* BIGDATA */
-		{
-			d = getnval(v->db, pag, n + 1);
-			value = sdbm_datum_copy(&d, &v->value);
-			restart = cur_mstamp != lru_wired_mstamp(v->db, pag);
-		}
 
-		/* FALL THROUGH */
-
-	restart_if_needed:
-		/*
-		 * If we detect a mismatch in the wired page modification stamp,
-		 * it means a concurrent update took place.
-		 *
-		 * We'll restart at the beginning, skipping keys we already
-		 * processed based on their hash.
-		 */
-
-		if G_UNLIKELY(restart) {
-			g_assert(!v->allkeys);	/* Otherwise we lock the page! */
-
-			/*
-			 * Record all the processed keys so far to ensure we do not
-			 * reprocess the same keys over and over.
-			 *
-			 * We do that now, when we know we have to restart, to not
-			 * add undue overhead in the regular case where no restart
-			 * will be required.
-			 */
-
-			while (n-- > 0) {
-				p = &pv[n];
-				hset_insert(v->seen, uint_to_pointer(p->khash));
-			}
-
-			/*
-			 * We need to refresh the page, but we limit ourselves to the
-			 * amount of key/value pairs that we had at the start of the
-			 * processing.  It's a loose iteration anyway.
-			 */
-
-			sdbm_synchronize(v->db);
-			cur_cnt = readpairv(pag, pv, cnt, TRUE);
-			cur_mstamp = lru_wired_mstamp(v->db, pag);
-			sdbm_unsynchronize(v->db);
-
-			if (1 == restarted)
-				v->stats->restarted++;
-
-			goto restart;
-		}
+		d = getnval(v->db, pag, n + 1);
+		value = sdbm_datum_copy(&d, &v->value);
 
 		/*
 		 * We now have the key and the value, safely copied in a private
@@ -333,6 +241,15 @@ restart:
 
 		processed++;
 		v->stats->items++;
+
+		/*
+		 * Relinquish lock before invoking the callback unless they want
+		 * to see all the keys or we restarted too many times already
+		 * and now proceed with the page locked at all times.
+		 */
+
+		if (!locked)
+			sdbm_unsynchronize(v->db);
 
 		if (v->type->deleting) {
 			if ((*v->type->u.cbr)(*key, *value, v->arg)) {
@@ -349,14 +266,18 @@ restart:
 				 * update though.
 				 */
 
-				sdbm_synchronize(v->db);
-
-				if G_LIKELY(cur_mstamp == lru_wired_mstamp(v->db, pag))
+				if (locked) {
 					r = sdbm_delete(v->db, *key);
-				else
-					refused = TRUE;
+				} else {
+					sdbm_synchronize(v->db);
 
-				sdbm_unsynchronize(v->db);
+					if G_LIKELY(cur_mstamp == lru_wired_mstamp(v->db, pag))
+						r = sdbm_delete(v->db, *key);
+					else
+						refused = TRUE;
+
+					sdbm_unsynchronize(v->db);
+				}
 
 				if G_UNLIKELY(refused) {
 					kept++;
@@ -384,6 +305,7 @@ restart:
 						loose_deleted(pv, cur_cnt, n);
 						cur_cnt--;		/* One less pair to process */
 						n--;			/* Stay at same index in next loop */
+						deleted = TRUE;	/* In case we restart below */
 					}
 				}
 			} else {
@@ -396,13 +318,63 @@ restart:
 			kept++;
 			v->stats->kept++;
 		}
+
+		if (!locked)
+			sdbm_synchronize(v->db);
+
+		/*
+		 * If we detect a mismatch in the wired page modification stamp,
+		 * it means a concurrent update took place.  This is possible
+		 * only if we released the lock on the database, naturally.
+		 *
+		 * We'll restart at the beginning, skipping keys we already
+		 * processed based on their hash.
+		 */
+
+		restart = !locked && cur_mstamp != lru_wired_mstamp(v->db, pag);
+
+		if G_UNLIKELY(restart) {
+			g_assert(!v->allkeys);	/* Otherwise we lock the page! */
+
+			if (deleted && n != cur_cnt - 1)
+				n++;		/* Undo n-- above since we processed that key */
+
+			/*
+			 * Record all the processed keys so far to ensure we do not
+			 * reprocess the same keys over and over.
+			 *
+			 * We do that now, when we know we have to restart, to not
+			 * add undue overhead in the regular case where no restart
+			 * will be required.
+			 */
+
+			while (n-- > 0) {
+				p = &pv[n];
+				hset_insert(v->seen, uint_to_pointer(p->khash));
+			}
+
+			/*
+			 * We need to refresh the page, but we limit ourselves to the
+			 * amount of key/value pairs that we had at the start of the
+			 * processing.  It's a loose iteration anyway.
+			 */
+
+			cur_cnt = readpairv(pag, pv, cnt, TRUE);
+			cur_mstamp = lru_wired_mstamp(v->db, pag);
+
+			if (1 == restarted)
+				v->stats->restarted++;
+
+			goto restart;
+		}
 	}
 
 	WFREE_ARRAY(pv, cnt);
+
+	assert_sdbm_locked(v->db);
+
 	if (locked)
 		sdbm_synchronize_yield(v->db);	/* Let other threads breathe */
-	else
-		sdbm_synchronize(v->db);		/* Regrab lock we had on entry */
 
 	return kept;
 }
