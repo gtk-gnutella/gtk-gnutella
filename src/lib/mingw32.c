f0ffd13b36c34cb26d9e3a03981358dce147f16a
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2015-11-08 11:41:34 +0100

    mingw_win2posix(): mapped ERROR_BAD_EXE_FORMAT to ENOEXEC.

--- src/lib/mingw32.c
+++ src/lib/mingw32.c
@@ -527,6 +527,8 @@ mingw_win2posix(int error)
 	case ERROR_ENVVAR_NOT_FOUND:
 		/* Got this error writing to a closed stdio fd, opened via pipe() */
 		return EBADF;
+	case ERROR_BAD_EXE_FORMAT:
+		return ENOEXEC;
 	default:
 		/* Only allocate once VMM layer has been initialized */
 		if (NULL == warned && mingw_vmm_inited) {
