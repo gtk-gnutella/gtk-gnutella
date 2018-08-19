79c77848eae3c04ae57efc924e9d9d9b422effd1
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2018-08-19 22:00:36 +0200

    Reduce memory footprint of compiled patterns for short patterns.

--- src/lib/pattern.c
+++ src/lib/pattern.c
@@ -47,10 +47,27 @@ enum cpattern_magic { CPATTERN_MAGIC = 0x3e074c43 };
 
 struct cpattern {				/**< Compiled pattern */
 	enum cpattern_magic magic;	/**< Magic number */
-	bool duped;					/**< Was `pattern' strdup()'ed? */
+	uint duped:1;				/**< Was `pattern' strdup()'ed? */
+	uint d8bits:1;				/**< If true, then delta is a uint8 array */
 	const char *pattern;		/**< The pattern */
 	size_t len;					/**< Pattern length */
-	size_t delta[ALPHA_SIZE];	/**< Shifting deltas */
+	/*
+	 * If the paatern is smaller than 255 chars, then the delta array is
+	 * really:
+	 *
+	 * 	  uint8 delta[ALPHA_SIZE];
+	 *
+	 * Otherwise, we allocate it as:
+	 *
+	 * 	  size_t delta[ALPHA_SIZE];
+	 *
+	 * Since most of the patterns will be small, this helps keeping the
+	 * deltas in a small memory region (more cache-friendly).  It also
+	 * reduces the memory footprint for compiled patterns.
+	 *
+	 * To know how the array is dimensionned, look at the small attribute.
+	 */
+	void *delta;				/**< Shifting deltas: array[ALPHA_SIZE]  */
 };
 
 static inline void
@@ -99,19 +116,21 @@ pattern_len(const cpattern_t *p)
  */
 
 /**
- * Build the shifting deltas table for the pattern.
+ * Build the shifting deltas small table for the pattern.
  *
  * @param p		the pattern to fill
  *
  * @return its argument
  */
 static cpattern_t *
-pattern_build_delta(cpattern_t *p)
+pattern_build_delta_small(cpattern_t *p)
 {
-	size_t plen, i, *pd;
+	size_t plen, i;
 	const uchar *c;
+	uint8 *pd;
 
 	pattern_check(p);
+	g_assert(p->d8bits);
 
 	plen = p->len + 1;		/* Avoid increasing within the loop */
 	pd = p->delta;
@@ -130,6 +149,89 @@ pattern_build_delta(cpattern_t *p)
 }
 
 /**
+ * Build the shifting deltas large table for the pattern.
+ *
+ * @param p		the pattern to fill
+ *
+ * @return its argument
+ */
+static cpattern_t *
+pattern_build_delta_large(cpattern_t *p)
+{
+	size_t plen, i, *pd;
+	const uchar *c;
+
+	pattern_check(p);
+	g_assert(!p->d8bits);
+
+	plen = p->len + 1;		/* Avoid increasing within the loop */
+	pd = p->delta;
+
+	for (i = 0; i < ALPHA_SIZE; i++)
+		*pd++ = plen;
+
+	plen--;			/* Restore original pattern length */
+
+	c = cast_to_constpointer(p->pattern);
+
+	for (pd = p->delta, i = 0; i < plen; c++, i++)
+		pd[*c] = plen - i;
+
+	return p;
+}
+
+/**
+ * Build the shifting deltas table for the pattern.
+ *
+ * @param p		the pattern to fill
+ *
+ * @return its argument
+ */
+static cpattern_t *
+pattern_build_delta(cpattern_t *p)
+{
+	pattern_check(p);
+
+	if (p->d8bits)
+		return pattern_build_delta_small(p);
+
+	return pattern_build_delta_large(p);
+}
+
+/**
+ * Allocate the delta[ALPAH_SIZE] array.
+ */
+static void
+pattern_delta_alloc(cpattern_t *p)
+{
+	pattern_check(p);
+
+	if (p->len < MAX_INT_VAL(uint8)) {
+		p->d8bits = TRUE;
+		p->delta = walloc(ALPHA_SIZE * sizeof(uint8));
+	} else {
+		p->d8bits = FALSE;
+		p->delta = walloc(ALPHA_SIZE * sizeof(size_t));
+	}
+}
+
+/**
+ * Free the delta[ALPHA_SIZE] array.
+ */
+static void
+pattern_delta_free(cpattern_t *p)
+{
+	pattern_check(p);
+
+	if (p->d8bits)
+		wfree(p->delta, ALPHA_SIZE * sizeof(uint8));
+	else
+		wfree(p->delta, ALPHA_SIZE * sizeof(size_t));
+
+	p->delta = NULL;
+}
+
+/**
  * Compile given string pattern by computing the delta shift table.
  * The pattern string given is duplicated.
  *
@@ -145,6 +247,7 @@ pattern_compile(const char *pattern)
 	p->pattern = xstrdup(pattern);
 	p->len = strlen(p->pattern);
 	p->duped = TRUE;
+	pattern_delta_alloc(p);
 
 	return pattern_build_delta(p);
 }
@@ -166,6 +269,7 @@ pattern_compile_fast(const char *pattern, size_t plen)
 	p->pattern = pattern;
 	p->len = plen;
 	p->duped = FALSE;
+	pattern_delta_alloc(p);
 
 	return pattern_build_delta(p);
 }
@@ -182,6 +286,7 @@ pattern_free(cpattern_t *p)
 		xfree(deconstify_gchar(p->pattern));
 		p->pattern = NULL; /* Don't use XFREE_NULL b/c of lvalue cast */
 	}
+	pattern_delta_free(p);
 	p->magic = 0;
 	WFREE(p);
 }
@@ -198,6 +303,50 @@ pattern_free_null(cpattern_t **cpat_ptr)
 		pattern_free(p);
 		*cpat_ptr = NULL;
 	}
+}
+
+/**
+ * Are we getting a match, given our word matching constraints?
+ *
+ * @param p		compiled pattern
+ * @param tp	pointer of substring match within text
+ * @param text	the text on which we are attempting a match
+ * @param tlen	the length of the text
+ * @param word	the word matching constraint
+ */
+static bool G_HOT
+pattern_has_matched(const cpattern_t *p, const char *tp,
+	const char *text, size_t tlen, qsearch_mode_t word)
+{
+	bool at_start = FALSE;	/* At beginning of word? */
+
+	if (word == qs_any)
+		return TRUE;		/* Start of substring */
+
+	/*
+	 * They set `word', so we must look whether we are at the start
+	 * of a word, i.e. if it is either the beginning of the text,
+	 * or if the character before is a non-alphanumeric character.
+	 */
+
+	g_assert(word == qs_begin || word == qs_whole);
+
+	if G_UNLIKELY(tp == text) {				/* At beginning of text */
+		if (word == qs_begin) return TRUE;
+		else at_start = TRUE;
+	} else if (is_ascii_space(*(tp-1))) {	/* At word boundary */
+		if (word == qs_begin) return TRUE;
+		else at_start = TRUE;
+	}
+
+	if (at_start && word == qs_whole) {
+		if (&tp[p->len] == text + tlen)		/* At end of string */
+			return TRUE;
+		else if (is_ascii_space(tp[p->len]))
+			return TRUE; 					/* At word boundary after */
+	}
+
+	return FALSE;	/* No match */
 }
 
 /**
@@ -222,6 +371,7 @@ pattern_qsearch(
 	const char *end;		/* End of text (first byte after physical end) */
 	size_t i;				/* Position within pattern string */
 	size_t plen;
+	bool d8bits = cpat->d8bits;
 
 	pattern_check(cpat);
 
@@ -238,39 +388,19 @@ pattern_qsearch(
 			if (*p != *t)
 				break;				/* Mismatch, stop looking here */
 
-		if (i == plen) {			/* OK, we got a pattern match */
-			bool at_begin = FALSE;
-
-			if (word == qs_any)
-				return tp;			/* Start of substring */
-
-			/*
-			 * They set `word', so we must look whether we are at the start
-			 * of a word, i.e. if it is either the beginning of the text,
-			 * or if the character before is a non-alphanumeric character.
-			 */
-
-			g_assert(word == qs_begin || word == qs_whole);
-
-			if (tp == text) {					/* At beginning of text */
-				if (word == qs_begin) return tp;
-				else at_begin = TRUE;
-			} else if (is_ascii_space(*(tp-1))) {	/* At word boundary */
-				if (word == qs_begin) return tp;
-				else at_begin = TRUE;
-			}
-
-			if (at_begin && word == qs_whole) {
-				if (&tp[plen] == end)			/* At end of string */
-					return tp;
-				else if (is_ascii_space(tp[plen]))
-					return tp; /* At word boundary after */
-			}
-
-			/* Fall through */
+		if G_UNLIKELY(i == plen) {	/* OK, we got a pattern match */
+			if (pattern_has_matched(cpat, tp, text, tlen, word))
+				return tp;
+			/* FALL THROUGH */
 		}
 
-		tp += cpat->delta[(uchar) tp[plen]]; /* Continue search there */
+		if (d8bits) {
+			const uint8 *d = cpat->delta;
+			tp += d[(uchar) tp[plen]];	/* Continue search there */
+		} else {
+			const size_t *d = cpat->delta;
+			tp += d[(uchar) tp[plen]];	/* Continue search there */
+		}
 	}
 
 	return NULL;		/* Not found */
