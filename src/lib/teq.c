5dd040a417a6ff073da36a5a1a2ba48f9f48e03d
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2020-04-05 18:45:01 +0200

    TEQ: optimize I/O events by posting them directly to I/O queue.
    Instead of posting "safe" events to the main thread into its TEQ queue,
    where it will be processed by a thread signal handler, to be requeued to
    the I/O queue for later processing, put them directly into the I/O queue
    of the main thread.
    
    This has two benefits:
    
    1. It is more efficient by reducing processing and shortening the delay
       before the event can be processed.
    
    2. It allows "unique" events to be posted and processed correctly. Sure,
       "unique" events were not posted twice to the TEQ queue, but they
       could pile up in the I/O queue because, by then, we have lost the
       information that they were meant to be "unique"!
    
    Hence we catch two birds with one stone.

--- src/lib/teq.c
+++ src/lib/teq.c
@@ -173,6 +173,13 @@ tevent_is_plain(const struct tevent * const tev)
 		THREAD_EVENT_IO_MAGIC == tev->magic;
 }
 
+static inline bool
+tevent_is_io(const struct tevent * const tev)
+{
+	return THREAD_EVENT_IO_MAGIC == tev->magic ||
+		THREAD_EVENT_IRPC_MAGIC == tev->magic;
+}
+
 /**
  * Magic numbers for thread event queue objects share the leading 24 bits.
  */
@@ -552,7 +559,9 @@ teq_ev_cmp(const void *a, const void *b)
 static bool
 teq_put(struct teq *teq, void *ev, bool unique)
 {
+	struct teq_io *teq_io;
 	bool posted = TRUE;
+	eslist_t *q;
 
 	teq_check(teq);
 	tevent_check(ev);
@@ -560,43 +569,58 @@ teq_put(struct teq *teq, void *ev, bool unique)
 	/* We only support "unique" for plain events */
 	g_assert(implies(unique, tevent_is_plain(ev)));
 
-	TEQ_LOCK(teq);
-
-	if G_UNLIKELY(unique && NULL != eslist_find(&teq->queue, ev, teq_ev_cmp))
-		posted = FALSE;
-
-	if (posted)
-		eslist_append(&teq->queue, ev);
-
-	TEQ_UNLOCK(teq);
-
-	if (posted)
-		thread_kill(teq->stid, TSIG_TEQ);
-
-	return posted;
-}
-
-/**
- * Add event to the I/O queue, signalling the I/O event loop.
- */
-static void
-teq_io_enqueue(struct teq *teq, void *ev)
-{
-	struct teq_io *teq_io = TEQ_IO(teq);
-
-	g_assert(teq_io != NULL);	/* If NULL, cast failed so wrong type */
-
-	TEQ_LOCK(teq);
-	eslist_append(&teq_io->ioq, ev);
-	TEQ_UNLOCK(teq);
-
 	/*
-	 * This will trigger an I/O event in the event loop, causing the
-	 * teq_io_callback() to be invoked to process the events inserted
-	 * in the I/O queue.
+	 * If we're posting an I/O event, it has to be sent to an I/O queue.
+	 * This was checked by our caller, and therefore it is an assertion here
+	 * that the cast of the `teq' argument to an I/O queue works.
+	 *
+	 * The I/O queue is usually installed in the main thread, the one running
+	 * the event loop and dispatching callbacks.  The advantage of going through
+	 * that and not through the regular queue is that it will be processed from
+	 * the main dispatching loop, at the top of the calling stack, which means
+	 * less opportunities to have the event processed anywhere in the targetted
+	 * thread running context (where it is processed through a thread signal
+	 * handler, in a safe place, but withoout any guarantee about which calls
+	 * we are already in).
 	 */
 
-	waiter_signal(teq_io->w);
+	if (tevent_is_io(ev)) {
+		teq_io = TEQ_IO(teq);
+		g_assert(teq_io != NULL);	/* If NULL, cast failed so wrong type */
+		q = &teq_io->ioq;			/* Selects the I/O queue */
+	} else {
+		teq_io = NULL;
+		q = &teq->queue;			/* Regular queue */
+	}
+
+	TEQ_LOCK(teq);
+
+	if G_UNLIKELY(unique && NULL != eslist_find(q, ev, teq_ev_cmp))
+		posted = FALSE;
+	else
+		eslist_append(q, ev);
+
+	TEQ_UNLOCK(teq);
+
+	if (posted) {
+		if (teq_io != NULL) {
+			/*
+			 * This will trigger an I/O event in the event loop, causing the
+			 * teq_io_callback() to be invoked to process the events inserted
+			 * in the I/O queue.
+			 */
+			waiter_signal(teq_io->w);
+		} else {
+			/*
+			 * The thread signal handler for TSIG_TEQ was set to teq_handle()
+			 * and will be executed as soon as the thread checks its signals,
+			 * whenever it enters our thread runtime.
+			 */
+			thread_kill(teq->stid, TSIG_TEQ);
+		}
+	}
+
+	return posted;
 }
 
 /**
@@ -753,18 +777,15 @@ teq_process(struct teq *teq)
 			goto next;
 		case THREAD_EVENT_IRPC_MAGIC:		/* Asynchronous "safe" RPC */
 		case THREAD_EVENT_IO_MAGIC:			/* Asynchronous "safe" routine */
-			{
-				/*
-				 * Simply move the event to the I/O queue, which will be
-				 * processed later from the main I/O event loop.
-				 */
-
-				teq_io_enqueue(teq, ev);
-			}
-			goto next;
+			/*
+			 * These events are directly enqueued into the I/O queue part
+			 * of the teq_io structure, hence they cannot appear in the
+			 * regular queue.
+			 */
+			g_assert_not_reached();
 		}
 
-		g_assert_not_reached();
+		g_assert_not_reached();		/* Unhandled case, impossible! */
 
 	next:
 		/*
@@ -1263,10 +1284,10 @@ teq_io_process(struct teq *teq)
 		tm_now_exact(&start);
 
 	/*
-	 * Consume all the events enqueued in our queue.
+	 * Consume all the events enqueued in our I/O queue.
 	 *
-	 * We're only dealing with THREAD_EVENT_IRPC_MAGIC events here, since
-	 * they are the only kind that gets redirected to that I/O queue.
+	 * We're only dealing with I/O events here, since they are the only
+	 * kind that gets redirected to that I/O queue.
 	 */
 
 	while (NULL != (ev = teq_io_remove(teq_io))) {
