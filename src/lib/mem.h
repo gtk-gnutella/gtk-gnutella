f1e797b9ed5eda54dc199581404306be8bc38e9b
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2020-09-22 10:15:01 +0200

    Let mem_test() run only once.
    Also added mem_validity_testable() and mem_protection_testable()
    to determine from the outside whether it is worth attempting to
    check for memory validity or page protection.

--- src/lib/mem.h
+++ src/lib/mem.h
@@ -55,6 +55,8 @@ bool mem_is_writable(const void *p);
 bool mem_is_writable_range(const void *p, size_t len);
 
 void mem_test(void);
+bool mem_validity_testable(void);
+bool mem_protection_testable(void);
 
 /*
  * Convenience aliases, for symetry with mem_is_writable().
