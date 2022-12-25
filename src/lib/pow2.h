c22eae36fdba6b184e1cbd7bbf78c0d5475e0182
Thomas Klausner <wiz@gatalith.at>
2022-12-25 18:58:56 +0100

    Detect and use popcount if it's available.

--- src/lib/pow2.h
+++ src/lib/pow2.h
@@ -67,6 +67,7 @@ is_pow2(uint32 value)
  *
  * @return number of 1 bits in a 32-bit integer.
  */
+#ifndef HAS_POPCOUNT
 static inline ALWAYS_INLINE G_CONST int
 popcount(uint32 x)
 #ifdef HAS_BUILTIN_POPCOUNT
@@ -85,6 +86,7 @@ popcount(uint32 x)
 	return (((x + (x >> 4)) & 0xf0f0f0f) * 0x1010101) >> 24;
 }
 #endif	/* HAS_BUILTIN_POPCOUNT */
+#endif	/* HAS_POPCOUNT */
 
 /**
  * Count trailing zeroes in a 32-bit integer, -1 for zero.
